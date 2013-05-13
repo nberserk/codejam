d4ff8442b037506e3b9aee9e36e80abc39416f75
Darren Ha <darren.ha@samsung.com>
2013-05-13 21:36:55 +0900

    r1a bullseye

--- src/codejam2013/r1a/Bullseye.java
+++ src/codejam2013/r1a/Bullseye.java
@@ -1,64 +1,128 @@
 package codejam2013.r1a;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
-import codejam.lib.CodejamBase;
+/*
+ * https://code.google.com/codejam/contest/2418487/dashboard#s=p0
+ */
 
-public class Bullseye extends CodejamBase {
+public class Bullseye {
+	BufferedWriter mWriter;
+	int mCurrentProblem;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Bullseye b = new Bullseye();
-		String dir = "./src/codejam2013/r1a/";
-		b.solve(dir + "A-ex.in", dir + "A-ex.out");
-	}
-	
-	@Override
-	public void parseAProblem(BufferedReader reader) {
+	public void solve(String inFile, String outFile) {
+		BufferedReader reader = null;
+		// long start = System.currentTimeMillis();
+
 		try {
-			String[] values = reader.readLine().split(" ");
-			double r = Double.parseDouble(values[0]);
-			double t = Double.parseDouble(values[1]);
+			reader = new BufferedReader(new FileReader(inFile));
+			mWriter = new BufferedWriter(new FileWriter(outFile));
 
-			r = 10000000000000000.0;
-			t = 1000000000000000000.0;
+			String str = reader.readLine();
+			int problemCount = Integer.parseInt(str);
+			for (int i = 1; i <= problemCount; i++) {
+				mCurrentProblem = i;
 
-			double lo = 1;
-			double hi = Math.pow(10, 19);
-			// hi = 100;
-			
-			double ar = 2 * r + 1;
+				String[] v = reader.readLine().split(" ");
+				long r = Long.parseLong(v[0]);
+				long t = Long.parseLong(v[1]);
+				// parse a problem
+				// solveSmallProblem();
 
-			while (lo < hi) {
-
-				double mid = (lo + hi) / 2;
-				double s = mid * (2 * ar + (mid - 1) * 4) / 2;// - sr;
-				if (s > t) {
-					hi = mid;
-				} else {
-					lo = mid;
-				}
-
-				if (Math.abs(hi - lo) < 0.0000001) {
-					break;
-				}
+				long c = solveLargeProblem(r, t);
+				writeSolution(String.format("%d", c));
 			}
-			int count = (int) Math.round(lo);
-			if (count - lo >= 0.000001) {
-				count -= 1;
-			}
-			String out = String.format("%d, %f", count, lo);
-			writeSolution(out);
 
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			try {
+				reader.close();
+			} catch (Exception e2) {
+				System.out.println(e2);
+			}
+			try {
+				mWriter.flush();
+				mWriter.close();
+			} catch (Exception e2) {
+				System.out.println(e2);
+
+			}
+		}
+
+		// long elapsedTimeMillis = System.currentTimeMillis()-start;
+		// float elapsedTimeSec = elapsedTimeMillis/1000F;
+		// System.out.println("elapsed time(sec) :"+ elapsedTimeSec);
+	}
+
+	private long solveLargeProblem(long r, long t) {
+
+		long s = 0;
+		long hi = 1;
+		do {
+			hi *= 2;
+		} while (hi * (2 * r + 2 * hi - 1) < t);
+
+		long lo = 1;
+
+		while (lo < hi) {
+			if (hi - lo == 1) {
+				break;
+			}
+			long mid = (hi + lo) / 2;
+			s = mid * (2 * r + 2 * mid - 1);
+			if (s < t) {
+				lo = mid + 1;
+			} else if (s > t) {
+				hi = mid - 1;
+			} else {
+				return mid;
+			}
+		}
+
+		long out = lo;
+		s = hi * (2 * r + 2 * hi - 1);
+		if (s <= t) {
+			out = hi;
+		}
+
+		return out;
+	}
+
+	private void solveSmallProblem() {
+
+	}
+
+	public void writeSolution(String s) {
+		String outStr = "Case #" + mCurrentProblem + ": " + s + "\n";
+		print(outStr);
+		try {
+			mWriter.write(outStr);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+	}
+
+	public static void print(String str) {
+		System.out.println(str);
+	}
+
+	public static void main(String[] args) {
+
+		// if ((Long.MAX_VALUE > 2000000000000000000l)) {
+		// print("");
+		// }
+
+		Bullseye b = new Bullseye();
+		String dir = "./src/codejam2013/r1a/";
+		String fn = "A-large-practice.in";
+		// long r = b.solveAProblem(5, 2, new int[] { 1, 2 });
+		// System.out.println(r);
+		b.solve(dir + fn, dir + fn.replace(".in", ".out"));
 	}
 
 }
