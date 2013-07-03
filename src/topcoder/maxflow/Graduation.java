74fb3340b093826d6f49e00f74e7272da78e8857
Darren Ha <darren.ha@samsung.com>
2013-07-03 13:52:48 +0900

    first max flow problem solved
    Signed-off-by: Darren Ha <darren.ha@samsung.com>

---
+++ src/topcoder/maxflow/Graduation.java
@@ -0,0 +1,18 @@
+package topcoder.maxflow;
+
+/*
+ * http://community.topcoder.com/stat?c=problem_statement&pm=2852&rd=5075
+ */
+public class Graduation {
+
+	public String moreClasses(String classesTaken, String[] requirements) {
+
+		return "0";
+	}
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
