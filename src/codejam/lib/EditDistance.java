c3187c38e9411cbc762f118d0ee71d348794575c
Darren Ha <darren.ha@samsung.com>
2013-11-28 16:25:12 +0900

    EditDistance suffixbased, prefixbased, simplify logic add boundary values

--- src/codejam/lib/EditDistance.java
+++ src/codejam/lib/EditDistance.java
@@ -5,87 +5,75 @@ import java.util.Arrays;
 
 
 public class EditDistance {
-
-	private static int sCostReplace;
-	private static int sCostDelete;
-	private static int sCostInsert;
+    private static boolean sIsDebug = true;
+    private static int sCostReplace = 1;
+    private static int sCostDelete = 1;
+    private static int sCostInsert = 1;
 	private static int[][] sCache;
 
+	static void debugPrint(String s) {
+		if (sIsDebug) {
+			System.out.println(s);
+		}
+	}
+
+    static void test(String from, String to) {
+        long time = System.currentTimeMillis();
+        int distance = editDistanceSuffix(from, to);
+        String log = String.format("%s->%s:%d (%dms) using suffix", from, to,
+                distance,
+                (System.currentTimeMillis() - time));
+        System.out.println(log);
+        time = System.currentTimeMillis();
+        distance = editDistancePrefix(from, to);
+        log = String.format("%s->%s:%d (%dms) using prefix", from, to,
+                distance,
+                (System.currentTimeMillis() - time));
+        System.out.println(log);
+        time = System.currentTimeMillis();
+        distance = editDistanceRecursive(from, to);
+        log = String.format("%s->%s:%d (%dms) using recursive\n", from, to,
+                distance, (System.currentTimeMillis() - time));
+        System.out.println(log);
+    }
+
 	public static void main(String[] args) {
+        test("a", "a");
+        test("a", "b");
+        test("a", "ab");
+        test("hieroglyphology", "michaelangelo");
+        test("sunday", "saturday");
 
-		String from= "a";
-		String to= "a";
-		System.out.println(from + "->" + to + ":" + editDistance(from, to));
-		System.out.println(from + "->" + to + ":"
-				+ editDistanceRecursive(from, to));
-
-		// from= "a";
-		// to= "b";
-		// System.out.println(from + "->" + to + ":" + editDistance(from, to));
-		// System.out.println(from + "->" + to + ":"
-		// + editDistanceRecursive(from, to, 0, 0));
-
-		// from= "a";
-		// to= "ab";
-		// System.out.println(from + "->" + to + ":" + editDistance(from, to));
-		// System.out.println(from + "->" + to + ":"
-		// + editDistanceRecursive(from, to, 0, 0));
-
-		from = "hieroglyphology";
-		to = "michaelangelo";
-		// from = "sunday";
-		// to = "saturday";
-		long time= System.currentTimeMillis();
-		int distance= editDistance(from, to);
-		String log= String.format("%s->%s:%d (%dms)", from, to, distance,
-				(System.currentTimeMillis() - time));
-		System.out.println(log);
-		time= System.currentTimeMillis();
-		distance= editDistanceRecursive(from, to);
-		log= String.format("%s->%s:%d (%dms)", from, to, distance,
-				(System.currentTimeMillis() - time));
-		System.out.println(log);
+        test("abcdefghij", "aaabbbbccccddddtt");
 	}
 
 	static int editDistanceRecursive2(String from, String to, int fromIndex,
 			int toIndex) {
 		int cost = Integer.MAX_VALUE-10;		
-		if (fromIndex >= from.length() || toIndex >= to.length()) {
-			return cost;
+        if (fromIndex >= from.length()) {
+            return sCostInsert * (to.length() - toIndex);
 		}
+
+        if (toIndex >= to.length()) {
+            return sCostDelete * (from.length() - fromIndex);
+        }
 
 		// cache hit ?
 		if (sCache[fromIndex][toIndex] != -1) {
 			return sCache[fromIndex][toIndex];
-		}
-		
+        }
 
-		if (fromIndex == from.length() - 1 && toIndex == to.length() - 1) {
-			if (from.charAt(fromIndex) == to.charAt(toIndex)) {
-				cost= 0;
-			} else {
-				cost= sCostReplace;
-			}
-		} else {
-			int parentR= editDistanceRecursive2(from, to, fromIndex + 1,
-					toIndex + 1);
-			int parentD= editDistanceRecursive2(from, to, fromIndex + 1,
-					toIndex);
-			int parentI= editDistanceRecursive2(from, to, fromIndex,
-					toIndex + 1);
-			if (from.charAt(fromIndex) == to.charAt(toIndex)) {
-				cost= Math.min(parentR, parentD);
-				cost= Math.min(cost, parentI);
+        boolean isMatch = from.charAt(fromIndex) == to.charAt(toIndex) ? true
+                : false;
 
-			} else {
-				parentR+= sCostReplace;
-				parentD+= sCostDelete;
-				parentI+= sCostInsert;
+        int parentR = editDistanceRecursive2(from, to, fromIndex + 1,
+                toIndex + 1) + (isMatch ? 0 : sCostReplace);
+        int parentD = editDistanceRecursive2(from, to, fromIndex + 1, toIndex)
+                + sCostDelete;
+        int parentI = editDistanceRecursive2(from, to, fromIndex, toIndex + 1)
+                + sCostInsert;
 
-				cost= Math.min(parentR, parentD);
-				cost= Math.min(cost, parentI);
-			}
-		}
+        cost = Math.min(Math.min(parentR, parentD), parentI);
 
 		if (sCache[fromIndex][toIndex] == -1) {
 			sCache[fromIndex][toIndex]= cost;
@@ -95,7 +83,6 @@ public class EditDistance {
 	}
 
 	static int editDistanceRecursive(String from, String to) {
-
 		sCache= new int[from.length()][to.length()];
 		for (int i= 0; i < sCache.length; i++) {
 			Arrays.fill(sCache[i], -1);
@@ -104,98 +91,116 @@ public class EditDistance {
 		return editDistanceRecursive2(from, to, 0, 0);
 	}
 
-	static int editDistance(String from, String to) {
-		sCostReplace= 1;
-		sCostDelete= 1;
-		sCostInsert= 1;
+    static int editDistancePrefix(String from, String to) {
+        int m = from.length() + 1;
+        int n = to.length() + 1;
 
-		int m= from.length();
-		int n= to.length();
+        int[][] t = new int[m][n];
+        Point[][] p = new Point[m][n];
+        char[][] p2 = new char[m][n];
+
+        // base cases
+        for (int i = 0; i < n; i++) {
+            t[0][i] = sCostInsert * i;
+        }
+        for (int i = 0; i < m; i++) {
+            t[i][0] = sCostDelete * i;
+        }
+
+        // start from 1
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                boolean matched = from.charAt(i - 1) == to.charAt(j - 1) ? true
+                        : false;
+
+                int d = sCostDelete + t[i - 1][j];
+                int in = sCostInsert + t[i][j - 1];
+                int r = t[i - 1][j - 1];
+                r += matched ? 0 : sCostReplace;
+
+                t[i][j] = Math.min(Math.min(d, in), r);
+
+                if (t[i][j] == d) {
+                    p[i][j] = new Point(i - 1, j);
+                    p2[i][j] = 'd';
+                } else if (t[i][j] == in) {
+                    p[i][j] = new Point(i, j - 1);
+                    p2[i][j] = 'i';
+
+                } else if (t[i][j] == r) {
+                    p[i][j] = new Point(i - 1, j - 1);
+                    p2[i][j] = matched ? ' ' : 'r';
+                }
+                // debugPrint(i + "," + j + " = " + t[i][j]);
+            }
+        }
+
+        // print path
+        Point cur = new Point(m - 1, n - 1);
+        while (true) {
+            char op = p2[cur.x][cur.y];
+            cur = p[cur.x][cur.y];
+            if (cur == null) {
+                break;
+            }
+            // debugPrint(String.format("%c (%d, %d)", op, cur.x, cur.y));
+        }
+
+        return t[m - 1][n - 1];
+    }
+
+	static int editDistanceSuffix(String from, String to) {
+        int m = from.length() + 1;
+        int n = to.length() + 1;
 
 		int[][] t= new int[m][n];
 		Point[][] p= new Point[m][n];
-		char[][] p2 = new char[m][n];
-		for (int i= m - 1; i >= 0; i--) {
-			for (int j= n - 1; j >= 0; j--) {
+		char[][] p2= new char[m][n];
 
+        // base cases
+        for (int i = 0; i < n; i++) {
+            t[m - 1][i] = sCostInsert * (n - 1 - i);
+        }
+        for (int i = 0; i < m; i++) {
+            t[i][n - 1] = sCostDelete * (m - 1 - i);
+        }
+
+        // start from m-2
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
 				boolean matched= from.charAt(i) == to.charAt(j) ? true : false;
 
-				if (i == m - 1 && j == n - 1) {
-					if (matched) {
-						p2[i][j] = ' ';
-						t[i][j] = 0;
-					} else {
-						p2[i][j] = 'r';
-						t[i][j] = sCostReplace;
-					}
-					p[i][j] = null;
-					continue;
-				}
+                int d = sCostDelete + t[i + 1][j];
+                int in = sCostInsert + t[i][j + 1];
+                int r = t[i + 1][j + 1];
+                r += matched ? 0 : sCostReplace;
 
-				int d= sCostDelete;
-				int in= sCostInsert;
-				int r= sCostReplace;
+                t[i][j] = Math.min(Math.min(d, in), r);
 
-				if (from.charAt(i) == to.charAt(j)) {
-					d= in= r= 0;
+                if (t[i][j] == d) {
+                    p[i][j] = new Point(i + 1, j);
+                    p2[i][j] = 'd';
+                } else if (t[i][j] == in) {
+                    p[i][j] = new Point(i, j + 1);
+                    p2[i][j] = 'i';
 
-				}
-
-				int min= Integer.MAX_VALUE;
-				if (i + 1 < m) {
-					d += t[i+1][j];
-					
-					if (d < min) {
-						min = d;
-						p[i][j] = new Point(i + 1, j);
-
-					}					
-				}
-				if (j + 1 < n) {
-					in += t[i][j+1];
-					if (in < min) {
-						min = in;
-						p[i][j] = new Point(i, j + 1);
-
-					}					
-				}
-				if (j + 1 < n && i + 1 < m) {
-					r += t[i + 1][j + 1] ;
-					if (r < min) {
-						min = r;
-						p[i][j] = new Point(i + 1, j + 1);
-					}
-				}
-
-				if (matched) {
-					p2[i][j] = ' ';
-				} else {
-					if (min == d) {
-						p2[i][j] = 'd';
-					} else if (min == in) {
-						p2[i][j] = 'i';
-					} else if (min == r) {
-						p2[i][j] = 'r';
-					} else {
-						System.out.println("strnage");
-					}
-				}
-
-				t[i][j]= min;
-				System.out.println(i + "," + j + "=" + t[i][j]);
+                } else if (t[i][j] == r) {
+                    p[i][j] = new Point(i + 1, j + 1);
+                    p2[i][j] = matched ? ' ' : 'r';
+                }
+                // debugPrint(i + "," + j + " = " + t[i][j]);
 			}
-		}
-		
+        }
 
+        // print path
 		Point cur = new Point(0, 0);
 		while (true) {
-			if (cur.x == m - 1 && cur.y == n - 1) {
-				break;
-			}
-
 			char op = p2[cur.x][cur.y];
 			cur = p[cur.x][cur.y];
-			System.out.println(String.format("%c %s", op, cur));
+            if (cur == null) {
+                break;
+            }
+            // debugPrint(String.format("%c (%d, %d)", op, cur.x, cur.y));
 		}
 
 		return t[0][0];
