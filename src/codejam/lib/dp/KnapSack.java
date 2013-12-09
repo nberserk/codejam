8ef4faa6a7a1d287e4abd429f6c9c8095d72634a
Darren Ha <darren.ha@samsung.com>
2013-12-09 10:26:29 +0900

    add recursive of Knapsack

--- src/codejam/lib/dp/KnapSack.java
+++ src/codejam/lib/dp/KnapSack.java
@@ -3,23 +3,26 @@ package codejam.lib.dp;
 public class KnapSack {
 	public static void main(String[] args) {
 
-		int max = solve(50, new int[] { 10, 20, 30 },
-				new int[] { 60, 100,
-				120 });
-		System.out.println(max);
-
-		max = solve(100, new int[] { 10, 20, 30, 40, 20 }, new int[] { 60, 100,
-				120, 10, 110 });
-		System.out.println(max);
+        solve(50, new int[] { 10, 20, 30 }, new int[] { 60, 100, 120 });
+        solve(100, new int[] { 10, 20, 30, 40, 20 }, new int[] { 60, 100, 120,
+                10, 110 });
 
 	}
 
-	public static int solve(int W, int[] weight, int[] value) {
-		int n = weight.length + 1;
-		int t[][] = new int[n][W + 1];
+    public static void solve(int S, int[] size, int[] value) {
+        int maxValue = solveTable(S, size, value);
+        System.out.println(maxValue + " <- using table");
+
+        maxValue = solveRecursive(S, size, value, 0);
+        System.out.println(maxValue + " <- using recursive");
+    }
+
+	public static int solveTable(int S, int[] size, int[] value) {
+		int n = size.length + 1;
+		int t[][] = new int[n][S + 1];
 
 		// prefix based
-		for (int i = 0; i < W; i++) {
+		for (int i = 0; i < S; i++) {
 			t[n - 1][i] = 0;
 		}
 		for (int i = 0; i < n; i++) {
@@ -27,22 +30,32 @@ public class KnapSack {
 		}
 
 		for (int i = n - 2; i >= 0; i--) {
-			for (int j = W; j >= 0; j--) {
+			for (int j = S; j >= 0; j--) {
 				t[i][j] = t[i + 1][j];
-				if (j - weight[i] >= 0) {
-					t[i][j] = Math.max(t[i][j], t[i + 1][j - weight[i]]
+				if (j - size[i] >= 0) {
+					t[i][j] = Math.max(t[i][j], t[i + 1][j - size[i]]
 							+ value[i]);
 				}
-				System.out.println(i + "," + j + "=" + t[i][j]);
+                // System.out.println(i + "," + j + "=" + t[i][j]);
 			}
 		}
 
-
-		return t[0][W];
+		return t[0][S];
 	}
 
-	public static int solveRecursive(int S, int[] weight, int[] value) {
-		return 1;
+    public static int solveRecursive(int S, int[] size, int[] value, int i) {
+        int cost = 0;
+        if (i >= size.length) {
+            return 0;
+        }
+
+        cost = solveRecursive(S, size, value, i + 1);
+        if (S >= size[i]) {
+            cost = Math.max(cost,
+                    solveRecursive(S - size[i], size, value, i + 1) + value[i]);
+        }
+
+        return cost;
 	}
 
 }
