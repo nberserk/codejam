ba6afeea13df939cf10974da04d8ef55d36e4fbd
Darren ha <nberserk@gmail.com>
2014-01-16 22:56:48 +0900

    update prim

--- src/codejam/lib/PrimMST.java
+++ src/codejam/lib/PrimMST.java
@@ -8,13 +8,36 @@ public class PrimMST {
 
 		int[][] m = new int[][] { { 0, 2, 0, 6, 0 }, { 2, 0, 3, 8, 5 },
 				{ 0, 3, 0, 0, 7 }, { 6, 8, 0, 0, 9 }, { 0, 5, 7, 9, 0 }, };
-
 		int n = PrimMST(m);
+		System.out.println(n);
+
+		m = new int[][] { 
+				{ 0, 4, 0, 0, 0, 0, 0, 8, 0 }, // 0
+				{ 4, 0, 8, 0, 0, 0, 0, 11, 0 }, // 1
+				{ 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+				{ 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+				{ 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+				{ 0, 0, 4, 14, 10, 0, 2, 0, 0 }, // 5
+				{ 0, 0, 0, 0, 0, 2, 0, 1, 6 }, // 6
+				{ 8, 11, 0, 0, 0, 0, 1, 0, 7 }, // 7
+				{ 0, 0, 2, 0, 0, 0, 6, 7, 0 },
+		};
+
+		n = PrimMST(m);
 		System.out.println(n);
 	}
 
 	public static int PrimMST(int[][] mat) {
 		int V = mat.length;
+
+		// validate matrix
+		for (int i = 0; i < V; i++) {
+			for (int j = 0; j < V; j++) {
+				if (mat[i][j] != mat[j][i]) {
+					assert false;
+				}
+			}
+		}
 
 		boolean[] mstSet = new boolean[V];
 		int[] key = new int[V];
@@ -37,11 +60,17 @@ public class PrimMST {
 
 			int u = minIndex;
 			min = Integer.MAX_VALUE;
+			System.out.println(i);
 			for (int j = 0; j < V; j++) {
-
-				if (mat[u][j] != 0 && mstSet[j] == false && mat[u][j] < key[j]) {
+				if (mat[u][j] == 0) {
+					continue;
+				}
+				if (mstSet[j] == false && mat[u][j] < key[j]) {
+					System.out.println(j + " is updated to " + mat[u][j]
+							+ ", was " + key[j]);
 					key[j] = mat[u][j];
 					parent[j] = u;
+
 				}
 			}
 		}
