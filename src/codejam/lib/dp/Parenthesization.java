7d091c8c2657480e95b5f17243f37664ba4481c6
Darren ha <nberserk@gmail.com>
2014-01-19 23:16:19 +0900

    parenthesization

---
+++ src/codejam/lib/dp/Parenthesization.java
@@ -0,0 +1,52 @@
+package codejam.lib.dp;
+
+public class Parenthesization {
+
+	public static void main(String[] args) {
+		Parenthesization p = new Parenthesization();
+		Matrix[] m = new Matrix[] { new Matrix(2, 3), new Matrix(3, 4),
+				new Matrix(4, 5) };
+
+		p.solve(m);
+
+	}
+
+	private void solve(Matrix[] m) {
+
+		int result = findMin(m, 0, m.length-1);
+		System.out.println(result);
+	}
+
+	/*
+	 * start, end : index based
+	 */
+	private int findMin(Matrix[] m, int start, int end) {
+		if (start==end) {
+			return 0;
+		}
+		if (end-start==1) {
+			return m[start].row*m[end].col*m[start].col; 
+		}
+		
+		int min = Integer.MAX_VALUE;
+		for (int i = start + 1; i <= end; i++) {
+			int cur = findMin(m, start, i);
+			cur += m[start].row * m[i].col * m[start].col;
+			cur += findMin(m, i, end);
+			if (cur < min) {
+				min = cur;
+			}
+		}
+		return min;
+	}
+
+	static class Matrix {
+		int row;
+		int col;
+
+		public Matrix(int r, int c) {
+			row = r;
+			col = c;
+		}
+	}
+}
