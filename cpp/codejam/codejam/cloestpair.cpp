b8978cdea65146297f3e325efbe78908d94a8cc7
Darren Ha <darren.ha@samsung.com>
2014-01-28 15:35:22 +0900

    add cpp project

---
+++ cpp/codejam/codejam/cloestpair.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  codejam
+//
+//  Created by darren on 2014. 1. 28..
+//  Copyright (c) 2014년 darren. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
