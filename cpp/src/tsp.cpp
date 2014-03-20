009cd85be2728559dc5900f27f6d9afd45c5a048
Darren Ha <darren.ha@samsung.com>
2014-03-20 18:53:25 +0900

    tsp added

--- cpp/src/tsp.cpp
+++ cpp/src/tsp.cpp
@@ -1,20 +1,41 @@
+
 #include <stdio.h>
 #include <math.h>
+#include <limits.h>
 
 #define MAX_N 10
+int N=4;
+int cache[MAX_N][1024];
 
-int t[10][];
+int tsp(int cost[4][4], int n, int end){
+	int min = INT_MAX, cur;
+    //int n = powl(2, N);
 
-int tsp(int cost[4][4], int N){
+	if(n==0){
+		return cost[end][0];
+	}
 
-    int n = powl(2, N-1);
+	int i, mask;
+
+	for(i=0;i<N;i++){
+		mask = 1<<i;
+		int temp = n&mask;
+		if(temp == mask){
+			cur = cost[end][i] + tsp(cost, n-mask, i);
+			if(cur < min){
+				min = cur;
+			}
+		}		
+	}
 
 
+	printf("%d,%d=%d\n", end,n,min);
+	return min;
 }
 
 
 
-void tsp_main(){
+int main(){
     int c[4][4] = {
             {0, 12, 11, 16},
             {15, 0, 15, 10},
@@ -22,6 +43,6 @@ void tsp_main(){
             {9,11,17,0}
     };
 
-    int min = tsp(c, 4);
+    int min = tsp(c, pow((long double)2, 4) -2, 0);
     printf("%d\n", min);
 }
