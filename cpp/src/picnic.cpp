1414773e78c7c7c379afb50d67e1151f9f47108d
Darren ha <nberserk@gmail.com>
2014-04-24 00:09:21 +0900

    picnic done

--- cpp/src/picnic.cpp
+++ cpp/src/picnic.cpp
@@ -16,6 +16,8 @@ int N, M;
 int count;
 typedef vector<pair<int,int>> vp;
 vector<pair<int,int>> pairs;
+#define MAX 10
+bool n[10];
 
 bool contain(vector<pair<int,int>> made, pair<int,int> cur){
     int size = made.size();
@@ -37,40 +39,45 @@ void printVP(vp p){
 }
 
 
-void numMate(vector<pair<int, int>> made, int mIdx){
-    if (mIdx>= M) {
-        if (made.size() == N/2) {
-            printVP(made);
-            ::count ++;
+void numMate(bool n[MAX]){
+    int i;
+    for( i=0; i<N; i++){
+        if (n[i]==false) {
+            break;
         }
+    }
+    if (i==N) {
+        ::count++;
         return;
     }
-    
-    pair<int,int> p;
-    for(int i=mIdx; i<pairs.size(); i++){
-        p = pairs[i];
-        if (contain(made, p) ) {
-            numMate(made, i+1);
-        }else{
-            vp cp(made);
-            cp.push_back(p);
-            numMate(cp, i+1);
 
+    pair<int,int> p;
+    int start = i;
+    for(int i=0; i<pairs.size(); i++){
+        p = pairs[i];
+        if (p.first == start && n[p.second] == false) {
+            n[p.first] = true;
+            n[p.second] = true;
+            numMate(n );
+            n[p.first] = false;
+            n[p.second] = false;
             
         }
-
+        
     }
 }
 
-void wrapper(int n, int m, vp p){
+void wrapper(int n2, int m, vp p){
     pairs = p;
-    N = n;
+    N = n2;
+    for (int i=0; i<N; i++) {
+        n[i] = false;
+    }
     M = m;
     pairs = p;
     ::count = 0;
 
-    vp made;
-    numMate(made, 0);
+    numMate(n);
     cout << ::count << endl;
 }
 
@@ -86,10 +93,23 @@ int main(int argc, const char * argv[])
     pair.push_back( make_pair(0, 1));
     pair.push_back( make_pair(1, 2));
     pair.push_back( make_pair(2, 3));
-    pair.push_back( make_pair(3, 0));
+    pair.push_back( make_pair(0, 3));
     pair.push_back( make_pair(0, 2));
     pair.push_back( make_pair(1, 3));
     wrapper(4, 6, pair);
+    
+    pair.clear();
+    pair.push_back( make_pair(0, 1));
+    pair.push_back( make_pair(0, 2));
+    pair.push_back( make_pair(1, 2));
+    pair.push_back( make_pair(1, 3));
+    pair.push_back( make_pair(1, 4));
+    pair.push_back( make_pair(2, 3));
+    pair.push_back( make_pair(2, 4));
+    pair.push_back( make_pair(3, 4));
+    pair.push_back( make_pair(3, 5));
+    pair.push_back( make_pair(4, 5));
+    wrapper(6, 10, pair);
     
     return 0;
 }
