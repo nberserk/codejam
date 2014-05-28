a02b3f21e3320d204efd222ca0eea038abed7f63
Darren ha <nberserk@gmail.com>
2014-05-28 23:36:22 +0900

    quantize 80%

--- cpp/codejam/quantize.cpp
+++ cpp/codejam/quantize.cpp
@@ -10,29 +10,93 @@
 #include <string.h>
 #include <limits>
 
+
 using namespace std;
+#define H_MAX 987654321;
 bool gDebug;
 int n, s;
 int a[100];
-int cache[10000];
+int cache[10][100];
 
 typedef long long ll;
 
+int calcError(int s, int e){
+    int size = e-s;
+    if(size==1)return 0;
+    int sum=a[s];
+    int i;
+    for( i=s+1;i<e;i++){
+        sum+=a[i];
+    }
+    float avg = sum/(size);
+    avg = floor(avg+0.5);
+    int iavg = (int)avg;
+    int err=0, temp;
+    for( i=s;i<e;i++){
+        temp = a[i]-iavg;
+        err += temp*temp;
+    }
 
-int solve(){
+    return err;
+}
 
-    int ret =0;
-    //printf("%d=%d\n", idx,ret);
+int solve(int region, int idx){
+
+    if(region==0){
+        return H_MAX;
+    }
+    if(idx>=n)
+        return H_MAX;
+
+    int& ret = cache[region][idx];
+    if(ret!=-1){
+        printf("hit cache : %d,%d=%d\n", region, idx,ret);
+        return ret;
+    }
+
+    ret = H_MAX;
+    if(region==1){
+        ret = calcError(idx, n);
+    }else{
+        int temp;
+        for(int e=idx+1;e<n;e++){
+            temp = calcError(idx,e);
+            ret = min(ret, temp+solve(region-1, e));
+        }        
+    }
+    
+    printf("%d,%d=%d\n", region, idx,ret);
     return ret;
 }
 
-void printStrange(){
-    printf("failed\n");
+void check(bool ret){
+    if (ret==false) {
+        printf("failed\n");
+    }
 }
 
 
 void test(){
     
+    n =1;
+    a[0] =4;
+    
+//    check(calcError(0, 1) ==0);
+//    check(solve(1,0) == 0);
+    
+    n=2;
+    a[1] = 2;
+    //check(calcError(0, 2) == 2);
+    
+    memset(cache, -1, sizeof(cache));
+    check(solve(1,0) ==2);
+    memset(cache, -1, sizeof(cache));
+    check(solve(2,0) == 0);
+    
+    n=3;
+    a[2] = 7;
+    memset(cache, -1, sizeof(cache));
+    check(solve(2, 0) ==2);
     
     
 }
@@ -58,10 +122,11 @@ int main(){
         for(j=0;j<n;j++){
             scanf("%d",a+j);
         }
+        std::sort(a, a+n);
 
         memset(cache,-1, sizeof(cache));
         //mark();
-        int maxV = solve(0);
+        int maxV = solve(s,0);
         //endMark();
         printf("%d\n", maxV);
     }
