fc51c13f6d1fe901fb32b17b721018ee20b8462c
Darren ha <nberserk@gmail.com>
2014-07-07 23:16:12 +0900

    klis ...

--- cpp/codejam/klis.cpp
+++ cpp/codejam/klis.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <time.h>
 #include <string>
+#include <sstream>
 #include <string.h>
 #include <stack>
 #include <limits>
@@ -24,35 +25,82 @@ void check(bool ret);
 
 bool gDebug;
 int gN, gK;
-int gSequence[500];
-ll gCache[501][501];
-int gParent[100][501];
+int gn[500];
+int gCache[501];
+int gCount[501];
 
 // 
-int lis(int start, int p){
+int lis(int start){
     if(start >=gN)
         return 0;
 
-    int& ret = gCache[start][p+1];
+    int& ret = gCache[start+1];
     if(ret!=-1)
         return ret;
 
-    ret = lis(start+1, p);
+    ret =1;
     int cur;
-    if(p==-1 || gSequence[start] > gSequence[p]){
-        cur = 1 + lis(start+1, start);
-        if(cur> ret){
-            ret = cur;
-        }
+    for( int i=start+1;i<gN;i++){
+        if(start==-1 || gn[i] > gn[start] ){
+            cur = 1+lis(i);
+            if(cur > ret){
+                ret = cur;
+            }
+        }            
     }
 
     return ret;
 }
 
-void solve(){
-    gN=1;
-    g
+void resetCache(){
+    memset(gCache, -1, sizeof(gCache));
+}
+
+int recon(int idx, int r){
+    if(r==0)
+        return 1;
+
+    int c = 0;
+    for(int i=idx+1;i<gN;i++){
+        if(gCache[i+1]==r-1 && gn[i] > gn[idx]){
+            c += recon(i+1, r-1);
+        }
+    }
+
+    gCount[idx] = c;
+    printf("gCount-%d=%d\n", idx, c);
+    return c;
+}
+
+void reconstruct(int i, string& r){
+    if (i>=gN)
+        return;
+
+    if(i!=-1){
+        stringstream ss;
+        ss << gn[i] << " ";
+        r += ss.str();
+    }
     
+    for( int j=i+1;j<gN;j++){
+        if(gCache[j+1] == gCache[i+1]-1){
+            reconstruct(j, r);
+            break;
+        }
+    }    
+}
+
+void solve(){
+    resetCache();
+    int v = lis(-1);
+
+    recon(-1, v);
+
+    string r;
+    reconstruct(-1, r);
+    
+    printf("%d\n", v-1);
+    printf("%s\n", r.c_str());
 }
 
 void check(bool ret){
@@ -69,7 +117,25 @@ void check(int expected, int actual){
 
 
 void test(){
-   
+    gN=1;
+    gn[0] =1;
+    resetCache();
+    check(1,lis( -1)-1);
+    
+    gN=2;
+    gn[1] = 2;
+    resetCache();
+    check(2, lis( -1)-1);
+    
+    gN=3;
+    gn[0] = 3;
+    gn[1] = 2;
+    gn[2]= 1;
+    resetCache();
+    check(1, lis(-1)-1);
+    
+    
+    
 }
 
 
@@ -84,14 +150,14 @@ int main(){
         fp = freopen(fn, "r", stdin);
     }
   
-    //test();
+    test();
     int count, p,j,k,n, i;
     scanf("%d", &count);
     
     for (p=0; p<count; p++) {
-        scanf("%d %d %d", &gN ,&gK);
+        scanf("%d %d", &gN ,&gK);
         for (i=0;i<gN;i++){
-            scanf("%d", gSequence+i);
+            scanf("%d", gn+i);
         }
         solve();
     }
