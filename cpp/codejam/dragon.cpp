1b606130a9d36509a9d54333782847fbe637f03f
Darren Ha <darren.ha@samsung.com>
2014-07-14 18:59:50 +0900

    dragon

--- cpp/codejam/dragon.cpp
+++ cpp/codejam/dragon.cpp
@@ -57,41 +57,53 @@ int dragon(string in, int generation)
 }
 
 // return kth char 
-void dragon_k(string in, int generation, int k)
-{
+char dragon_k(string in, int generation, int k)
+{    
     if (generation<0) {
-        return;
+        return 'z';
     }
+
+    //printf("%s\n",in.c_str());
     
     int half = gLength[generation];
+    int org_k = k;
     
     int size= in.size();
-    if(k<size){
-        printf("%c", in[k]);
-        return;
-    }
+    if(generation==0){
+        return in[k-1];
+    }    
     for(int i=0;i<size;i++){
         if(in[i]=='X'){
             if(k>half)
                 k-=half;
-            else
-                dragon_k("X+YF", generation-1, k);
+            else{
+                return dragon_k("X+YF", generation-1, k);                
+            }            
         }else if(in[i]=='Y'){
             if(k>half)
                 k-=half;
-            else
-                dragon_k("FX-Y", generation-1, k);            
+            else{
+                return dragon_k("FX-Y", generation-1, k);                
+            }            
         }else{
-            k--;            
+            k--;
+        }
+
+        if (k==0) {
+            //printf("%s, gen=%d,k=%d  --> %c\n", in.c_str(), generation, org_k, in[i]);
+            return in[i];            
         }
     }
+    
+    return 'z';
 }
 
 void solve(){
 
+    char c;
     for(int i=0;i<gL;i++){
-        dragon_k("FX", gN, gP+i-1);
-        printf("\n");
+        c = dragon_k("FX", gN, gP+i);
+        printf("%c", c);
     }    
     printf("\n");
 
@@ -109,10 +121,35 @@ void check(int expected, int actual){
     }
 }
 
+void check(char expected, char actual){
+    if (expected!=actual) {
+        printf("failed expected=%c, actual=%c\n", expected, actual);
+    }
+}
+
 
 void test(){
     
-    
+    check('F', dragon_k("FX",0,1));
+    check('X', dragon_k("FX",0,2));
+
+    check('F', dragon_k("FX", 1, 1));
+    check('X', dragon_k("FX", 1, 2));
+    check('+', dragon_k("FX", 1, 3));
+    check('Y', dragon_k("FX", 1, 4));
+    check('F', dragon_k("FX", 1, 5));
+
+    check('F', dragon_k("FX", 2, 1));
+    check('X', dragon_k("FX", 2, 2));
+    check('+', dragon_k("FX", 2, 3));
+    check('Y', dragon_k("FX", 2, 4));
+    check('F', dragon_k("FX", 2, 5));
+    check('+', dragon_k("FX", 2, 6));
+    check('F', dragon_k("FX", 2, 7));
+    check('X', dragon_k("FX", 2, 8));
+    check('-', dragon_k("FX", 2, 9));
+    check('Y', dragon_k("FX", 2, 10));
+    check('F', dragon_k("FX", 2, 11));
 }
 
 
@@ -125,18 +162,13 @@ int main(){
     FILE *fp;
     if (gDebug) {
         fp = freopen(fn, "r", stdin);
-    }
-  
-    //test();
-    int count, p,j,k,n, i;
-    scanf("%d", &count);
-
+    }  
+    
     // calc length cache
     int xy=1;
-    gLength[0] = 0;
-    
+    gLength[0] = 1;
     for(int i=1;i<51;i++){
-        gLength[i] = xy*3;
+        gLength[i] = gLength[i-1]+xy*3;
         if (gLength[i] >= 1000000000){
             gLength[i]=   1000000000;
             for (int j=i; j<51; j++) {
@@ -144,12 +176,16 @@ int main(){
             }
             break;
         }
-        
         //printf("%d,%d\n", gLength[i],xy);
         xy*=2;
     }
-    
-    
+
+    // test
+    //test();
+
+    // handling input
+    int count, p,j,k,n, i;
+    scanf("%d", &count);
     for (p=0; p<count; p++) {
         scanf("%d %d %d", &gN ,&gP, &gL);        
         solve();
