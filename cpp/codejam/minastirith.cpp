488e7395790a6512b2e55143217f0744d3cddd64
Darren ha <nberserk@gmail.com>
2014-08-17 23:48:30 +0900

    mina got intersection points

--- cpp/codejam/minastirith.cpp
+++ cpp/codejam/minastirith.cpp
@@ -21,12 +21,12 @@
 using namespace std;
 #define H_MAX 987654321
 #define H_MIN -987654321
-#define H_EPSILON 0.000001
+#define H_EPSILON 0.0000001
 #define H_MOD 10000000
+#define H_PI 3.14159265
 typedef long long ll;
 
 void check(bool ret);
-
 
 
 bool gDebug;
@@ -34,11 +34,44 @@ int gN;
 double gPoint[100][3];
 int gLen[100];
 
+// assume a!=0
+void calc(double a,double b, double r){
+    double t1 = r*r-a*a-b*b-64;
+    if (fabs(a)<H_EPSILON){
+        double y1=-t1/(2*b);
+        double x1 = sqrt(64-y1*y1);
+        double x2 = -x1;
 
+        printf("%f %f\n", x1, y1);
+        printf("%f %f\n", x2, y1);
+        return;           
+    }
+    
+    
 
+    double a1=1.0+b*b/(a*a);
+    double b1 = (b*t1)/(a*a);
+    double c1 = t1*t1/(4*a*a)-64;
+
+    double t2 = sqrt(b1*b1-4*a1*c1);    
+    double y1 = (-b1+t2)/(2*a1);
+    double y2 = (-b1-t2)/(2*a1);
+
+    double x1 = (t1+2*b*y1)/-(2*a);
+    double x2 = (t1+2*b*y2)/-(2*a);
+
+    printf("%f %f\n", x1, y1);
+    printf("%f %f\n", x2, y2);
+}
 
 int solve(){
+
+    for (int i=0; i<gN; i++) {
+        calc(gPoint[i][1], gPoint[i][0], gPoint[i][2]);
+    }
+
     
+    return 1;
 }
 
 
@@ -61,7 +94,17 @@ void check(char expected, char actual){
     }
 }
 
+void check(double expected, double actual){
+    if (fabs(expected - actual)>H_EPSILON) {
+        printf("failed expected=%lf, actual=%lf\n", expected, actual);
+    }
+}
+
 void test(){
+
+    check(45.0, atan(1.0)*180/H_PI);
+    check(-45.0, atan(-1.0)*180/H_PI);
+    
     
 }
 
@@ -84,9 +127,9 @@ int main(){
     for (p=0; p<count; p++) {        
         scanf("%d", &gN);
         for ( j = 0; j < gN; j++){            
-            scanf("%f", &gPoint[j][0]);
-            scanf("%f", &gPoint[j][1]);
-            scanf("%f", &gPoint[j][2]);
+            scanf("%lf", &gPoint[j][0]);
+            scanf("%lf", &gPoint[j][1]);
+            scanf("%lf", &gPoint[j][2]);
         }        
         int r = solve();        
         printf("%d\n", r);
