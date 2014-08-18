87539dfb1d1aa2692f8faed6560889199072357b
Darren ha <nberserk@gmail.com>
2014-08-18 23:32:40 +0900

    minas abort

--- cpp/codejam/minastirith.cpp
+++ cpp/codejam/minastirith.cpp
@@ -32,10 +32,42 @@ void check(bool ret);
 bool gDebug;
 int gN;
 double gPoint[100][3];
+double gAngle[100][2];
 int gLen[100];
 
+double getAngle(double x, double y){
+    if (fabs(x)<=H_EPSILON) {
+        if (y>=0) {
+            return 90;
+        }else
+            return 270;
+    }
+    if (fabs(y)<=H_EPSILON) {
+        if (x>=0) {
+            return 0;
+        }else
+            return 180;
+    }
+    
+    double ret;
+    if (x>=0) {
+        ret = atan(y/x)*180.0/H_PI;
+        if (ret<0) {
+            ret += 360;
+        }
+    }else{
+        if (y>0) {
+            ret = 180 - atan(-x/y)*180.0/H_PI ;
+        }else{
+            ret = 180 + atan(x/y)*180.0/H_PI ;
+        }
+    }
+    
+    return ret;
+}
+
 // assume a!=0
-void calc(double a,double b, double r){
+void calc(double a,double b, double r, int i){
     double t1 = r*r-a*a-b*b-64;
     if (fabs(a)<H_EPSILON){
         double y1=-t1/(2*b);
@@ -46,8 +78,6 @@ void calc(double a,double b, double r){
         printf("%f %f\n", x2, y1);
         return;           
     }
-    
-    
 
     double a1=1.0+b*b/(a*a);
     double b1 = (b*t1)/(a*a);
@@ -59,16 +89,26 @@ void calc(double a,double b, double r){
 
     double x1 = (t1+2*b*y1)/-(2*a);
     double x2 = (t1+2*b*y2)/-(2*a);
-
-    printf("%f %f\n", x1, y1);
-    printf("%f %f\n", x2, y2);
+    double angle[2];
+    angle[0] = getAngle(x1, y1);
+    angle[1] = getAngle(x2, y2);
+    
+    sort(angle, angle+1);
+    printf("%f %f\n", angle[0], angle[1]);
+    gAngle[i][0] = angle[0];
+    double aa = atan2(y1, x1)*180/H_PI;
+    gAngle[i][1] = angle[1];
+    double aa2 = atan2(y2, x2)*180/H_PI;
+    //printf("%f %f\n", x2, y2);
 }
 
 int solve(){
 
     for (int i=0; i<gN; i++) {
-        calc(gPoint[i][1], gPoint[i][0], gPoint[i][2]);
+        calc(gPoint[i][1], gPoint[i][0], gPoint[i][2], i);
     }
+    
+    
 
     
     return 1;
