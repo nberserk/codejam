25326b8849242e3eaba0262180d57b64258fbd1a
Darren Ha <darren.ha@samsung.com>
2014-09-01 11:37:07 +0900

    dic done. wow

--- cpp/codejam/dictionary.cpp
+++ cpp/codejam/dictionary.cpp
@@ -28,43 +28,39 @@ typedef long long ll;
 void check(bool ret);
 
 int gN;
-char gWord[200][30];
+char gWord[1000][30];
 bool gDebug;
 vector<vector<int>> gGraph;
-int gState[26]; //  1 : temp mark, 2 : visited
+int gState[26]; // 0:init  1 : temp mark, 2 : visited
 bool gVisited[26];
 vector<int> gOrder;
 
-void visit(int here){
-    
-}
-
-void toposort(){
-    memset(gState, 0, sizeof(gState));
-    for (int i=0; i<26; i++) {
-        if (gState[i]==0) {
-            visit(i);
-        }
-    }
-    
-}
-
-void dfsVisit(int here){
-    gVisited[here] = true;
-    for (int i = 0; i < 26; i++){
-        if (gGraph[here][i] && !gVisited[i])
-            dfsVisit(i);
-    }
-    gOrder.push_back(here);
-}
-
 void printVector(vector<int>& v){
     for (int i=0; i<v.size(); i++) {
-        printf("%d,", v[i]);
+        printf("%c", v[i]+'a');
     }
     printf("\n");
 }
 
+int visit(int here){
+    if (gState[here]==1){
+        return -1;
+    }
+    if (gState[here]==2){
+        return 0;
+    }
+
+    gState[here]=1;
+    for (int i = 0; i < 26; i++){
+        if (gGraph[here][i]){
+            if (visit(i)==-1)
+                return -1;
+        }
+    }
+    gState[here]=2;
+    gOrder.push_back(here);
+    return here;
+}
 void fillGraph(){
     gGraph = vector<vector<int>>(26, vector<int>(26, 0));
     int i;
@@ -82,57 +78,27 @@ void fillGraph(){
         }
     }
 }
-
-void topologicalSort(){
+void toposort(){
+    gOrder.clear();
+    memset(gState, 0, sizeof(gState));
     fillGraph();
     
-    gOrder.clear();
-    memset(gVisited, false, sizeof(gVisited));
+    bool cycle=false;
+    for (int i=0; i<26; i++) {
+        if (visit(i)==-1){
+            cycle=true;
+            break;
+        }
+    }
 
-    for (int i = 0; i < 26; i++){
-        bool hasEdge = false;
-        for (int j=0; j<26; j++) {
-            if (gGraph[i][j]) {
-                hasEdge=true;
-                break;
-            }
-        }
-        if (!hasEdge || gVisited[i]) {
-            continue;
-        }
-        dfsVisit(i);
-        //printVector(gOrder);
+    if (cycle){
+        printf("INVALID HYPOTHESIS\n");
+        return;
     }
 
     reverse(gOrder.begin(), gOrder.end());
-    
-    // check cycle
-    int size = gOrder.size();
-    for (int i=0; i<size; i++) {
-        for (int j=i+1; j<size; j++) {
-            if (gGraph[gOrder[j]][gOrder[i]]) {
-                printf("INVALID HYPOTHESIS\n");
-                return;
-            }
-        }
-    }
-    
-    bool used[26];
-    memset(used, false, sizeof(used));
-    for (int i=0; i<gOrder.size(); i++) {
-        printf("%c", gOrder[i]+'a');
-        used[gOrder[i]]=true;
-    }
-    for (int i = 0; i < 26; i++){
-        if (!used[i])
-            printf("%c", i+'a');
-    }
-    printf("\n");
+    printVector(gOrder);    
 }
-
-
-
-
 
 void check(bool ret){
     if (ret==false) {
@@ -178,8 +144,7 @@ int main(){
             scanf("%s", gWord[j]);
         }
 
-        topologicalSort();
-        
+        toposort();        
     }
     
     if (gDebug) {
