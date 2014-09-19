bc3ddd8fd6ee43b6c9ce7cd7b15a16d59a285743
Darren Ha <darren.ha@samsung.com>
2014-09-19 17:31:28 +0900

    memlimit

---
+++ java/src/crackcode/MemoryLimit.java
@@ -0,0 +1,12 @@
+package crackcode;
+
+public class MemoryLimit {
+
+    public static void main(String[] args) {
+        int n = 1 << 31;
+        System.out.println(Integer.toBinaryString(n));
+
+
+    }
+
+}
