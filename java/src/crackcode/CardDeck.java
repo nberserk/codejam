c0115ed747bceada34824662cb35f7e639090a48
Darren ha <nberserk@gmail.com>
2014-09-30 00:03:14 +0900

    CardDeck design done

---
+++ java/src/crackcode/CardDeck.java
@@ -0,0 +1,74 @@
+package crackcode;
+
+import java.util.ArrayList;
+
+public class CardDeck {
+	static class Card {
+		enum Value {
+			ace, two, three, four, ten, jack, queen, king
+		};
+
+		enum Shape {
+			heart, space, diamond, clover, joker
+		};
+
+		Card(Value v, Shape s) {
+			mValue = v;
+			mShape = s;
+		}
+
+		Shape mShape;
+		Value mValue;
+		public Shape getShape() {
+			return mShape;
+		}
+
+		public Value getValue() {
+			return mValue;
+		}
+	}
+
+	static class User {
+		private String mName;
+		ArrayList<Card> mCards = new ArrayList<Card>();
+
+		User(String name) {
+			mName = name;
+		}
+		void addCard(Card card) {
+			mCards.add(card);
+		}
+
+		void removeCard(Card c) {
+			mCards.remove(c);
+		}
+
+		String getName() {
+			return mName;
+		}
+
+		int getCardCount() {
+			return mCards.size();
+		}
+	}
+
+	ArrayList<Card> mRemainingCards = new ArrayList<Card>();
+
+	Card getNextCard() {
+		Card ret = mRemainingCards.get(mRemainingCards.size() - 1);
+		mRemainingCards.remove(mRemainingCards.size() - 1);
+		return ret;
+	}
+
+	void shuffle() {
+	}
+
+	public static void main(String[] args) {
+		CardDeck deck = new CardDeck();
+		deck.shuffle();
+
+		User darren = new User("darren");
+		darren.addCard(deck.getNextCard());
+	}
+
+}
---
+++ java/src/crackcode/HStackMin.java
@@ -0,0 +1,54 @@
+package crackcode;
+
+public class HStackMin {
+	int[] data = new int[1024];
+	int[] min = new int[1024];
+	int top;
+
+	void push(int v) {
+		data[top++] = v;
+		if (top==1) {
+			min[top - 1] = v;
+		} else {
+			min[top - 1] = Math.min(v, min[top - 2]);
+		}
+
+	}
+
+	int pop() {
+		if (top == 0) {
+			return -1;
+		}
+		return data[--top];
+	}
+
+	int getCurrentMin() {
+		if (top == 0) {
+			return -1;
+		}
+		return min[top - 1];
+	}
+
+	public static void main(String[] args) {
+		HStackMin s = new HStackMin();
+		CheckUtil.check(-1, s.pop());
+
+		s.push(100);
+		CheckUtil.check(100, s.getCurrentMin());
+		s.push(1);
+		CheckUtil.check(1, s.getCurrentMin());
+		s.push(5);
+		CheckUtil.check(1, s.getCurrentMin());
+		s.push(-5);
+		CheckUtil.check(-5, s.getCurrentMin());
+
+		CheckUtil.check(-5, s.pop());
+		CheckUtil.check(5, s.pop());
+		CheckUtil.check(1, s.pop());
+		CheckUtil.check(100, s.pop());
+		CheckUtil.check(-1, s.pop());
+		
+		System.out.println("HStack ended");
+	}
+
+}
