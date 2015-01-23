59041150b5b686e63239b9374330c1ec069c4666
Darren Ha <darren.ha@samsung.com>
2015-01-23 16:50:21 +0900

    cheerleader

---
+++ cpp/codejam/sotong/cheerleader.cpp
@@ -0,0 +1,119 @@
+#include <algorithm>
+#ifdef _MSC_VER
+#include <io.h>
+#define F_OK 0
+#else
+#include <unistd.h>
+#endif
+#include <stdio.h>
+#include <math.h>
+#include <vector>
+#include <map>
+#include <queue>
+#include <time.h>
+#include <string>
+#include <sstream>
+#include <string.h>
+#include <stack>
+#include <list>
+#include <limits>
+#include <set>
+#include <iostream>
+
+
+using namespace std;
+#define H_MAX 987654321
+#define H_MIN -987654321
+#define H_EPSILON 0.000001
+typedef long long ll;
+
+
+void check(bool ret);
+
+bool gDebug;
+int gN;
+char gS[11];
+
+
+
+void permutation(char* s, int c){
+    if (c==gN-1){
+        printf("%s\n", s);
+        return;
+    }
+
+    char t[11];
+    strcpy(t,s);
+
+    sort(s+c, s+gN);    
+    for (int i = c; i < gN; i++){                
+        if (c!=i && *(s+c)==*(s+i)){
+            continue;
+        }        
+        swap(*(s+c), *(s+i));
+        permutation(s, c+1);        
+        swap(*(s+c), *(s+i));        
+    }
+    strcpy(s, t);
+}
+
+
+
+void solve(){
+    sort(gS, gS+gN);
+    permutation(gS, 0);
+    printf("\n");
+}
+
+void check(bool ret){
+    if (ret==false) {
+        printf("failed\n");
+    }
+}
+
+void check(int expected, int actual){
+    if (expected!=actual) {
+        printf("failed expected=%d, actual=%d\n", expected, actual);
+    }
+}
+
+void check(char expected, char actual){
+    if (expected!=actual) {
+        printf("failed expected=%c, actual=%c\n", expected, actual);
+    }
+}
+
+void test(){
+}
+
+int main(){
+    string fn = __FILE__;
+    size_t pos = fn.find(".cpp");
+    fn = fn.substr(0,pos) + ".txt";    
+    if (access(fn.c_str(), F_OK)!=-1) {
+        gDebug = true;
+    }
+
+    FILE *fp;
+    if (gDebug) {
+        fp = freopen(fn.c_str(), "r", stdin);
+    }
+    
+    //test();
+    
+    // handling input
+    int count, p,j,k, i,n;
+    scanf("%d", &count);
+    for (p=0; p<count; p++) {        
+        scanf("%s", gS);
+        gN = strlen(gS);        
+        solve();
+    }
+    
+    if (gDebug) {
+        fclose(fp);
+    }
+    return 0;
+    
+}
+
