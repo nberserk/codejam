aa6b29dd026eca2c28f63e055242bf80268d7cb7
Darren Ha <nberserk@gmail.com>
2015-03-02 17:36:35 +0900

    connectingCore implement custom prority_queue

--- cpp/codejam/sotong/connectingCore.cpp
+++ cpp/codejam/sotong/connectingCore.cpp
@@ -40,15 +40,111 @@ public:
     }
 };
 
-void check(bool ret);
+void check(bool ret){
+    if (ret==false) {
+        printf("failed\n");
+    }
+}
+
+void check(int expected, int actual){
+    if (expected!=actual) {
+        printf("failed expected=%d, actual=%d\n", expected, actual);
+    }
+}
+
+void check(char expected, char actual){
+    if (expected!=actual) {
+        printf("failed expected=%c, actual=%c\n", expected, actual);
+    }
+}
 
 bool gDebug;
 int gN; // max 300x300
 int gMat[300][300];
 int gCore[100][2]; //0:x, 1:y
 int gCoreCount;
-Node q[100000];
+
 int gMin[100][100];
+
+
+// pq start
+
+
+int pq_size;
+Node q[100000];
+void pq_init(){    
+    pq_size=0;
+}
+
+void pq_swap(int a, int b){
+    Node t = q[a];
+    q[a] = q[b];
+    q[b] = t;
+}
+
+void pq_push(int x, int y, int v){
+
+    q[pq_size].x  = x;
+    q[pq_size].y = y;
+    q[pq_size].v = v;
+    pq_size++;
+    
+    if (pq_size==1){
+        return;
+    }    
+
+    //
+    int i=pq_size-1;    
+    while (i>0){
+        int p = (i-1)/2;
+        if (q[p].v > q[i].v){
+            pq_swap(p, i);
+        }else{
+            break;
+        }
+        i=p;
+    }    
+}
+
+void pq_heapify(int c){
+    int smallest=c;
+    int l=2*c+1;
+    int r=l+1;
+    if (l<pq_size && q[l].v<q[smallest].v){
+        smallest = l;
+    }
+    if (r<pq_size && q[r].v < q[smallest].v){
+        smallest=r;
+    }
+    if (smallest!=c){
+        pq_swap(c, smallest);
+        pq_heapify(smallest);
+    }    
+}
+
+Node pq_pop(){
+    pq_size--;
+    pq_swap(0, pq_size);
+    pq_heapify(0);
+    return q[pq_size];
+}
+
+
+void pq_print(){
+    int l=0;
+    int linebreak=1;
+    for (int i = 0; i < pq_size; i++){
+        printf("%2d ", q[i].v);
+        l++;
+        if (l==linebreak){
+            printf("\n");
+            linebreak*=2;
+            l=0;
+        }
+    }
+    printf("\n\n");
+}
+// pq end
 
 
 // using priority_queue is 3-4 times faster
@@ -110,6 +206,54 @@ int findMinUsingPriorityqueue(int s, int e){
     return ret;    
 }
 
+int findMinUsingCustomPriorityqueue(int s, int e){
+    int dx = gCore[e][0];
+    int dy = gCore[e][1];
+    
+    int visited[300][300] = {0,};
+    
+    pq_init();
+    pq_push(gCore[s][0], gCore[s][1], 1);
+    
+    int d[4][2] = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+    
+    int ret = 1000;
+    while (pq_size>0){
+        // TODO: use simple dijkstra
+        Node cur = pq_pop();
+        
+        // cache
+        if (visited[cur.y][cur.x]!=0 && cur.v >= visited[cur.y][cur.x]){
+            continue;
+        }
+        visited[cur.y][cur.x] = cur.v;
+        
+        //
+        for (int i = 0; i < 4; i++){
+            int nx = cur.x + d[i][0];
+            int ny = cur.y + d[i][1];
+            if (nx <0 || ny<0 || nx >=gN || ny>=gN) continue;
+            if (gMat[ny][nx]==-1){
+                continue;
+            }else if (gMat[ny][nx]==0){
+                if (nx==dx && ny==dy){
+                    return cur.v;
+                }else
+                    continue;
+            }
+            int nv = cur.v < gMat[ny][nx] ? gMat[ny][nx] : cur.v;
+            if (nv>ret){
+                continue;
+            }
+            pq_push(nx, ny, nv);
+            //            printf("%d\n", count);
+        }
+    }
+    
+    return ret;
+}
+
+
 int findMin(int s, int e){
     int dx = gCore[e][0];
     int dy = gCore[e][1];
@@ -169,6 +313,7 @@ struct Unit{
     int s,e,v;
 };
 Unit gUnit[100000];
+Unit gUnit2[100000];
 
 bool myCompare(const Unit& l, const Unit& r){
     return l.v - r.v;
@@ -193,19 +338,27 @@ void comparePerformance(){
     h_startTimeMeasure();
     for (int i = 0; i < gCoreCount; i++){
         for (int j = i+1; j < gCoreCount; j++){
-            gUnit[count].s = i;
-            gUnit[count].e = j;
-            gUnit[count].v = findMin(i,j);
+            gUnit2[count].s = i;
+            gUnit2[count].e = j;
+            gUnit2[count].v = findMinUsingCustomPriorityqueue(i,j);
             //printf("%d-%d=%d\n", i, j, gUnit[count].v);
             count++;
         }
     }
     h_endTimeMeasure();
 
+    // check each result
+    count=0;
+    for (int i = 0; i < gCoreCount; i++){
+        for (int j = i+1; j < gCoreCount; j++){
+            check(gUnit[count].v, gUnit2[count].v);
+            count++;
+        }
+    }
 }
 
 void solve(){
-    comparePerformance();
+    //comparePerformance();
     
     int count =0;
     for (int i = 0; i < gCoreCount; i++){
@@ -260,27 +413,25 @@ void solve(){
 }
 
 
-void check(bool ret){
-    if (ret==false) {
-        printf("failed\n");
-    }
-}
 
-void check(int expected, int actual){
-    if (expected!=actual) {
-        printf("failed expected=%d, actual=%d\n", expected, actual);
-    }
-}
-
-void check(char expected, char actual){
-    if (expected!=actual) {
-        printf("failed expected=%c, actual=%c\n", expected, actual);
-    }
-}
 
 
 
 void test(){
+    pq_init();
+    pq_push(0,0, 100);
+
+    pq_push(1,1, 99);
+    pq_push(1,1, 97);
+    pq_push(1,1, 50);
+    pq_print();
+
+    check(50, q[0].v);
+    check(4, pq_size);
+
+    pq_push(1,1,10);
+    check(10, q[0].v);
+    pq_print();
 }
 
 int main(){
