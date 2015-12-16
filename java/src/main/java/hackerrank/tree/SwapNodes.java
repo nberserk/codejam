ea29e1e73460249cd1c3d38e88921c03dd207e4c
Darren Ha <nberserk@gmail.com>
2015-12-16 19:35:04 +0900

    swapnodes ing

---
+++ java/src/main/java/hackerrank/tree/SwapNodes.java
@@ -0,0 +1,41 @@
+package hackerrank.tree;
+
+import java.util.Scanner;
+
+/**
+ * Created by darren on 2015. 12. 16..
+ */
+public class SwapNodes {
+
+    static class Node{
+        int v;
+        int left, right;
+
+        public Node(int v, int l, int r){
+            this.v = v;
+            left =l;
+            right=r;
+        }
+    }
+
+    Node nodes[] = new Node[1025];
+
+    void solve(int N, int K){
+
+        // init
+        for (int i = 0; i < N; i++) {
+            nodes[i].
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        
+
+
+    }
+
+
+}
