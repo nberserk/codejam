6dee9e22f89e15e97bd57cf12d5390fcdea11f81
Darren ha <nberserk@gmail.com>
2016-01-10 22:31:26 +0900

    efficient exponentiation phase2

---
+++ java/src/main/java/codejam/lib/impl/MajorityElement.java
@@ -0,0 +1,14 @@
+package main.java.codejam.lib.impl;
+
+/**
+ * Created by darren on 1/9/16.
+ */
+public class MajorityElement {
+
+    public static void main(String[] args) {
+
+        System.out.println(Math.floor(3.1));
+
+
+    }
+}
--- java/src/main/java/hackerrank/EfficientExponentiation.java
+++ java/src/main/java/hackerrank/EfficientExponentiation.java
@@ -1,97 +1,74 @@
 package hackerrank;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.TreeSet;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by darren on 2015. 12. 31..
  */
 public class EfficientExponentiation {
 
-    static int [][]cache = new int[280][3];
+    static List<Integer> best ;
 
-    public static void solve(int N, TreeSet<Integer> set){
-        if(N==1) return;
-
-        if(cache[N][0]!=0){
-            set.add(cache[N][0]);
-            set.add(cache[N][1]);
+    public static void solve(int N, int cur, LinkedList<Integer> list){
+        if (cur>N)
+            return;
+        if(best!=null && list.size()>best.size())
+            return;
+        if(cur==N){
+            if(best==null || best.size()>list.size()){
+                best = new LinkedList<Integer>(list);
+                //System.out.println(list.size()+ ":"+list);
+            }
             return;
         }
 
-        cache[N][2] = 999999999;
-        set.add(N);
-        int half = N/2;
-        for(int i=1;i<=half;i++){
-
-            int b =N-i;
-
-            TreeSet<Integer> next = new TreeSet<>();
-            solve(i, next);
-            //merge(set, next);
-
-            if(!set.contains(b)){
-                TreeSet<Integer> next2 = new TreeSet<>();
-                solve(b, next2);
-                merge(next, next2);
-            }
-
-            if(next.size()+1<cache[N][2]){
-                System.out.println(String.format("%d,%d-%d=%d",N, i,b,next.size()+1));
-                cache[N][0] = i;
-                cache[N][1] = b;
-                cache[N][2] = next.size()+1;
-                set.clear();
-                set.addAll(next);
-            }
+        for (int i = list.size()-1; i >=0; i--) {
+            int v = cur + list.get(i);
+            list.addLast(v);
+            solve(N, v, list);
+            list.removeLast();
         }
-    }
-
-    private static void merge(TreeSet<Integer> big, TreeSet<Integer> small) {
-        for (Integer i : small){
-            if (big.contains(i))
-                continue;
-            big.add(i);
-        }
-    }
-
-    static void path(int N, TreeSet<Integer> set){
-        if(N==1)return;
-        set.add(N);
-
-        path(cache[N][0], set);
-        path(cache[N][1], set);
     }
 
     static int solve2(int N){
-        return 0;
+        best=null;
+
+        LinkedList<Integer> list = new LinkedList<>();
+        list.addLast(1);
+        solve(N, 1, list);
+
+        System.out.println(best.size() - 1);
+        for (int i = 1; i < best.size(); i++) {
+            int t = best.get(i);
+            int prev = best.get(i-1);
+
+            for (int k = 0; k < i; k++) {
+                if ( prev + best.get(k) == t){
+                    System.out.println(String.format("n^%d * n^%d=n^%d", prev, best.get(k), t));
+                    break;
+                }
+            }
+        }
+        return best.size()-1;
     }
 
     public static void main(String[] args) {
-        cache[1][0] = 1;
-        cache[1][1] = 0;
-        cache[1][2] = 1;
 
-        cache[2][0] = 1;
-        cache[2][1] = 1;
-        cache[2][2] = 1;
+        assertEquals(1, solve2(2));
+        assertEquals(5, solve2(15));
 
-        TreeSet<Integer> set2 = new TreeSet<>();
-        solve(15, set2);
-        path(15, new TreeSet<Integer>());
 
         Scanner s = new Scanner(System.in);
-        int T= s.nextInt();
-        for(int i=0;i<T;i++){
-            int n = s.nextInt();
-            TreeSet<Integer> set = new TreeSet<Integer>();
-            solve(n, set);
+        int T = s.nextInt();
 
-            path(n, set);
-
-            System.out.println(set);
-
+        for (int i = 0; i < T; i++) {
+            int k = s.nextInt();
+            solve2(k);
         }
-
     }
 }
