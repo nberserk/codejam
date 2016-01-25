05a4b99414e69b4d63807268dc3185db99d9b5b9
Darren ha <nberserk@gmail.com>
2016-01-25 22:24:52 +0900

    revisite CoinChange

--- java/src/main/java/codejam/lib/dp/CoinChange.java
+++ java/src/main/java/codejam/lib/dp/CoinChange.java
@@ -1,6 +1,8 @@
-package topcoder.dp;
+package main.java.codejam.lib.dp;
 
 import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
 
 /*
  * http://www.geeksforgeeks.org/dynamic-programming-set-7-coin-change/
@@ -39,13 +41,39 @@ public class CoinChange {
 		return c;
 	}
 
+	int countIterative(int[] s, int m, int N){
+		int[][] t = new int[N+1][m];
+
+		for (int i = 0; i < m; i++) {
+			t[0][i]=1;
+		}
+
+		int x,y;
+		for (int i=1;i<=N;i++){
+			for (int j = 0; j < m; j++) {
+				// include s[j]
+				x = i-s[j]>=0 ? t[i-s[j]][j] : 0; // include s[j];
+				y = j-1>=0    ? t[i][j-1]: 0;
+
+				t[i][j] = x+y;
+			}
+		}
+
+		return t[N][m-1];
+	}
+
 	public static void main(String[] args) {
 		CoinChange c = new CoinChange();
-		int count = c.count(new int[] { 1, 2, 3 }, 2, 4);
+		int[] s = new int[] { 1, 2, 3 };
+		int count = c.count(s, 2, 4);
 		System.out.println(count);
+		assertEquals(count , c.countIterative(s, s.length, 4));
 
-		count = c.count(new int[] { 2, 5, 3, 6 }, 3, 10);
+
+		s = new int[] { 2, 5, 3, 6 };
+		count = c.count(s, s.length-1, 10);
 		System.out.println(count);
+		assertEquals(count, c.countIterative(s, s.length, 10));
 
 	}
 
