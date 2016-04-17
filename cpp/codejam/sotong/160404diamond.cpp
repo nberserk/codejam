7950d1c24d9b032b46e21b302d0cd0901ddf656d
Darren ha <nberserk@gmail.com>
2016-04-17 23:39:49 +0900

    diamond - bruteforce

--- cpp/codejam/sotong/160404diamond.cpp
+++ cpp/codejam/sotong/160404diamond.cpp
@@ -51,7 +51,7 @@ void build(){
     for (int z = 0; z < N; z++){
         for (int y = 0; y < N; y++){
             for (int x = 0; x < N; x++){
-                gCube[z][y][x] = rand()%10==0 ? 0 : 1;
+                gCube[z][y][x] = rand()%32==0 ? 0 : 1;
             }
         }
     }      
@@ -66,9 +66,67 @@ void print(int z){
     }
 }
 
+// return endZ having the region is 1.
+int findEndZ(int sx, int sy, int ex, int ey, int sz){
+    for (int z=sz; z<N;z++) {
+        for (int y = sy; y <= ey; y++){
+            for (int x = sx; x <= ex; x++){
+                if (gCube[z][y][x]==0){
+                    return z-1;
+                }                
+            }
+        }        
+    }
+
+    return N-1;
+}
+
+int bruteforce(){
+    int maxVolume=1;
+    
+    for (int z = 0; z < N; z++){
+        for (int y = 0; y < N; y++){
+            for (int x = 0; x < N; x++){
+                
+                
+                int max_x = N-1;
+                for (int ey = y; ey < N; ey++){
+                    for (int ex = x; ex <= max_x; ex++){
+                        if (gCube[z][ey][ex]==0) {
+                            max_x = ex-1;
+                            break;
+                        }
+
+                        printf("%d,%d - %d,%d\n", y,x, ey,ex);
+                        int ez = findEndZ(x,y, ex, ey, z+1);
+                        int w = ex-x+1;
+                        int h = ey-y+1;
+                        int d = ez-z+1;
+                        int v = w*h*d;
+                        if (v>maxVolume) {
+                            printf("maxVolume=%d\n", v);
+                            maxVolume=v;
+                        }
+                    }
+                    
+                    if (max_x==0) {
+                        break;
+                    }
+                }
+                // d
+            }
+                
+                
+        }
+        break;            
+    }
+
+    return maxVolume;
+}
+
 int test(){
     print(0);
-    return 0;
+    return bruteforce();
 }
 
 int main(){
