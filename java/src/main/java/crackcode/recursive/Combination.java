c4d1b08f6d97db57c0abc0b2a38d9231fcf8a38f
Darren ha <nberserk@gmail.com>
2016-09-24 09:09:22 +0900

    enhance combination

--- java/src/main/java/crackcode/recursive/Combination.java
+++ java/src/main/java/crackcode/recursive/Combination.java
@@ -1,23 +1,26 @@
 package main.java.crackcode.recursive;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by darren on 9/19/16.
  */
-public class Combination extends TestCase {
-
+public class Combination {
 
 
     static List<List<Integer>> combination(int[] a, int k){
         List<List<Integer>> out = new ArrayList<>();
 
         Arrays.sort(a);
-        combinationUtil(a, 0, k, out);
+        LinkedList<Integer> temp = new LinkedList<>();
+        combinationUtil(a, 0, k, temp, out);
         return out;
     }
 
@@ -27,35 +30,38 @@ public class Combination extends TestCase {
         a[j]=t;
     }
 
-    static private void combinationUtil(int[] a, int index, int k, List<List<Integer>> out) {
-        if(index==k){
-            List<Integer> list = new ArrayList<>();
-            for (int i = 0; i < k; i++) {
-                list.add(a[i]);
-            }
-            out.add(list);
+    static private void combinationUtil(int[] a, int start, int k, LinkedList<Integer> temp,  List<List<Integer>> out) {
+        if(temp.size()==k){
+            out.add(new ArrayList<Integer>(temp));
             return;
         }
 
-        for (int i = index; i < a.length; i++) {
-            if (index>0 && a[index-1]>=a[i])
-                continue;
-            swap(a, index, i);
-            combinationUtil(a, index+1, k, out);
-            swap(a, index, i);
-
-            // to remove duplicates
-            while(i<a.length-1 && a[i] == a[i+1])
-                i++;
+        for (int i = start; i < a.length; i++) {
+            if(i>start && a[i-1]==a[i]) continue; // remove duplicate
+            if(temp.size()>0 && a[i]<temp.peekLast()) continue; // always take same or larger one
+            temp.add(a[i]);
+            combinationUtil(a, i + 1, k, temp, out);
+            temp.removeLast();
         }
     }
 
 
-    public void testCombination(){
+    @Test
+    public void test(){
         int[] a = {3,2,2,1};
 //        int[] a = {3,2,1,4};
         List<List<Integer>> r = combination(a, 2);
 
+        assertEquals(4, r.size());
+
+        for (int i = 0; i < r.size(); i++) {
+            System.out.println(r.get(i));
+        }
+
+        int[] b = {1,1,1};
+        r= combination(b, 2);
+        assertEquals(1, r.size());
+        System.out.println("");
         for (int i = 0; i < r.size(); i++) {
             System.out.println(r.get(i));
         }
