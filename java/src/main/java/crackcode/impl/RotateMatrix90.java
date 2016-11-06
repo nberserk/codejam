22ee433b86246cc8d7e9d5b0b50c05a31b461798
Darren ha <nberserk@gmail.com>
2016-11-06 12:36:00 +0900

    RotateMatrix90 inplace done

---
+++ java/src/main/java/crackcode/impl/RotateMatrix90.java
@@ -0,0 +1,56 @@
+package main.java.crackcode.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * Created by darren on 11/6/16.
+ *
+ * https://leetcode.com/problems/rotate-image/
+ *
+ * You are given an n x n 2D matrix representing an image.
+
+ Rotate the image by 90 degrees (clockwise).
+
+ Follow up:
+ Could you do this in-place?
+ 
+ */
+public class RotateMatrix90 {
+    void swap(int[][]m, int x, int y, int dx, int dy){
+        int temp=m[y][x];
+        m[y][x]=m[dy][dx];
+        m[dy][dx]=temp;
+    }
+    public void rotate(int[][] matrix) {
+        int N=matrix.length;
+        if(N==0) return;
+
+        int half=N/2;
+        for(int sy=0;sy<half;sy++){
+            int sx=sy;
+            int ey=N-1-sy;
+            int ex=ey;
+
+            for(int x=sx;x<ex;x++){
+                int d=x-sx;
+                swap(matrix, x,sy,ex,sy+d);
+                swap(matrix, x,sy,ex-d,ey);
+                swap(matrix, x,sy,sx,ey-d);
+            }
+        }
+    }
+
+    @Test
+    public void test(){
+        int[][] a= {{1,2,3}, {4,5,6}, {7,8,9}};
+
+        int[][] sol={{7,4,1}, {8,5,2}, {9,6,3}};
+
+        rotate(a);
+        assertArrayEquals(sol[0], a[0]);
+        assertArrayEquals(sol[1], a[1]);
+        assertArrayEquals(sol[2], a[2]);
+    }
+}
---
+++ java/src/main/java/crackcode/tree/Rope.java
@@ -0,0 +1,90 @@
+package main.java.crackcode.tree;
+
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * from: http://www.careercup.com/question?id=24532662
+ *
+ * rope data structure?
+ *
+
+ You are given two array, first array contain integer which represent heights of persons and second array contain how many persons in front of him are standing who are greater than him in term of height and forming a queue. Ex
+ A: 3 2 1
+ B: 0 1 1
+ It means in front of person of height 3 there is no person standing, person of height 2 there is one person in front of him who has greater height then he, similar to person of height 1. Your task to arrange them
+ Ouput should be.
+ 3 1 2
+ Here - 3 is at front, 1 has 3 in front ,2 has 1 and 3 in front.
+
+ * idea :
+ * - make tree has count property. count is # left nodes(taller)
+
+ */
+
+public class Rope {
+	static class Node {
+		int value;
+		int count;
+		Node left, right;
+
+		Node(int v) {
+			value=v;
+			count=1;
+		}
+	}
+
+	// assume height is sorted by decreasing order
+	List<Integer> sortByCountTallerThanSelf(int[] height, int[] count){
+		Node root = new Node(height[0]);
+		for (int i = 1; i < height.length; i++) {
+			Node cur = new Node(height[i]);
+			insertNode(root, cur, count[i]);
+		}
+		List<Integer> ret = new ArrayList<>();
+		traverse(root, ret);
+
+		return ret;
+	}
+
+	private void traverse(Node node, List<Integer> ret) {
+		if(node==null) return;
+
+		traverse(node.left, ret);
+		ret.add(node.value);
+		traverse(node.right, ret);
+	}
+
+	private void insertNode(Node node, Node cur, int count) {
+		if (node.count > count){
+			node.count++;
+			if(node.left==null) {
+				node.left=cur;
+			}else insertNode(node.left, cur, count);
+		}else {
+			count-=node.count;
+			if(node.right==null){
+				node.right= cur;
+			}else insertNode(node.right, cur, count);
+		}
+	}
+
+	@Test
+	public void test(){
+		int[] h = {6, 5, 4, 3, 2, 1};
+		int[] c = {0, 0, 0, 2, 2, 4};
+
+		int[] h2 = {3, 2, 1};
+		int[] c2 = {0, 1, 1};
+
+		assertEquals("[3, 1, 2]", sortByCountTallerThanSelf(h2, c2).toString());
+		assertEquals("[6, 1, 3, 2, 5, 4]", sortByCountTallerThanSelf(h, c));
+
+	}
+
+}
