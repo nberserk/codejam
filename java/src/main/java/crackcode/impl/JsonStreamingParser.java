d1e8aa8c2b930f37c8061ab785e0ec32396d792a
Darren ha <nberserk@gmail.com>
2016-12-07 02:20:04 +0900

    streaming parser

--- java/src/main/java/crackcode/impl/JsonStreamingParser.java
+++ java/src/main/java/crackcode/impl/JsonStreamingParser.java
@@ -1,5 +1,7 @@
 package crackcode.impl;
 
+import org.junit.Test;
+
 import java.util.Stack;
 
 /**
@@ -10,14 +12,20 @@ public class JsonStreamingParser {
     enum State {
         STATE_START_DOCUMENT,
         STATE_IN_OBJECT,
-        STATE_IN_ARRAY
+        STATE_IN_ARRAY,
+        STATE_IN_STRING,
+        STATE_KEY,
+        STATE_ENDKEY,
     }
     Stack<State> stack = new Stack<>();
     State state = State.STATE_START_DOCUMENT;
     String cur="";
 
-
     private void parse(char c){
+
+        if ((c == ' ' || c == '\t' || c == '\n' || c == '\r')
+            && !(state!=State.STATE_IN_STRING ) )
+            return;
 
         switch (state){
             case STATE_START_DOCUMENT:
@@ -29,11 +37,50 @@ public class JsonStreamingParser {
             }
             break;
             case STATE_IN_OBJECT:
-
+                if(c=='"')
+                    startKey();
+                else if(c=='}')
+                    endObject();
+                break;
+            case STATE_IN_STRING:
+                if (c=='"')
+                    endString();
+                else
+                    cur+=c;
+                break;
+            case STATE_ENDKEY:
+                if(c==':'){
+                    startValue();
+                }
                 break;
 
-        }
 
+        }
+    }
+
+    private void startValue() {
+
+    }
+
+    private void endString() {
+        state = stack.pop();
+        if (state==State.STATE_KEY) {
+            System.out.println("key:" + cur);
+
+        }else
+            System.out.println("value:" + cur);
+        cur="";
+    }
+
+    private void endObject() {
+        System.out.println("endObject:" + cur);
+        state = stack.pop();
+    }
+
+    private void startKey() {
+        stack.push(state);
+        stack.push(State.STATE_KEY);
+        state = State.STATE_IN_STRING;
     }
 
     private void startArray() {
@@ -47,6 +94,11 @@ public class JsonStreamingParser {
         System.out.println("startObject");
         stack.push(state);
         state=State.STATE_IN_OBJECT;
+    }
+
+    @Test
+    public void test(){
+
     }
 
 }
