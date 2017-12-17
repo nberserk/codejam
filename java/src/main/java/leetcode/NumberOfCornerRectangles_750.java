a10b33f931a5d3a5d8ac694c85281f724bcebc96
Darren ha <nberserk@gmail.com>
2017-12-17 15:07:13 +0900

    number of corner rectangle done

--- java/src/main/java/leetcode/NumberOfCornerRectangles_750.java
+++ java/src/main/java/leetcode/NumberOfCornerRectangles_750.java
@@ -8,22 +8,15 @@ public class NumberOfCornerRectangles_750 {
         int R = grid.length;
         int C = grid[0].length;
 
-        int[][][] dp = new int[R][C][C];
+        int[][] dp = new int[C][C];
 
         int ret =0;
         for (int y = R-1; y >=0; y--) {
             for (int sx = 0; sx < C; sx++) {
                 for (int ex = sx+1; ex < C; ex++) {
                     if (grid[y][ex]==1 && grid[y][sx]==1){
-                        dp[y][sx][ex] = 1;
-                        if(y!=R-1 && dp[y+1][sx][ex]>0){
-                            ret += dp[y+1][sx][ex];
-                            dp[y][sx][ex]+=dp[y+1][sx][ex];
-                        }
-                    }else{
-                        if(y!=R-1){
-                            dp[y][sx][ex]=dp[y+1][sx][ex];
-                        }
+                        ret += dp[sx][ex];
+                        dp[sx][ex]= dp[sx][ex]+1;
                     }
                 }
             }
