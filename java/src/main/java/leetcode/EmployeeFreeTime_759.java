ed5d238eca306e82edeb5586855577cb6bd716b7
Darren ha <nberserk@gmail.com>
2018-01-15 23:04:52 +0900

    employee free time done

--- java/src/main/java/leetcode/EmployeeFreeTime_759.java
+++ java/src/main/java/leetcode/EmployeeFreeTime_759.java
@@ -1,6 +1,9 @@
 package leetcode;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -27,55 +30,37 @@ public class EmployeeFreeTime_759 {
     }
 
     public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
-        List<Interval> merge = schedule.get(0);
-        for(int i=1;i<schedule.size();i++){
-            merge = mergeInterval(merge, schedule.get(i));
+        ArrayList<Interval> all = new ArrayList<>();
+        for (List<Interval> l : schedule){
+            all.addAll(l);
         }
 
+        List<Interval> merge = mergeInterval(all);
 
-        return merge;
+        List<Interval> ret = new ArrayList<>();
+        for (int i = 0; i < merge.size() - 1; i++) {
+            ret.add(new Interval(merge.get(i).end, merge.get(i+1).start));
+        }
+        return ret;
     }
 
-    private List<Interval> mergeInterval(List<Interval> left, List<Interval> right) {
-        int L=left.size();
-        int R=right.size();
-        int l=0;
-        int r=0;
+    private List<Interval> mergeInterval(ArrayList<Interval> merged) {
+        Collections.sort(merged, (a,b)-> a.start==b.start?a.end-b.end:a.start-b.start );
+
         List<Interval> ret = new ArrayList<>();
-        while(l<L || r<R){
-            if(r == R) {
-                ret.add(left.get(l++));
-            } else if(l==L){
-                ret.add(right.get(r++));
+        Interval prev = merged.get(0);
+        for (int i = 1; i < merged.size(); i++) {
+            Interval cur = merged.get(i);
+            if (prev.end >= cur.start){
+                prev.end = Math.max(prev.end, cur.end);
+            }else if(prev.end < cur.start){
+                ret.add(prev);
+                prev = cur;
             }else{
-                Interval i=left.get(l);
-                Interval i2 = right.get(r);
-                boolean swap=false;
-                if(i.start>i2.start || (i.start==i2.start && i.end>i2.end)){
-                    Interval t = i;
-                    i=i2;
-                    i2=t;
-                    swap=true;
-                }
-                if(i.end < i2.start){
-                    ret.add(i);
-                    if (swap) r++;
-                    else l++;
-                }else if(i.end >= i2.start){
-                    i.end = Math.max(i.end, i2.end);
-                    if (swap) l++;
-                    else r++;
-                }else if(i2.end <= i.end){
-                    if(swap) l++;
-                    else r++;
-                }else if (i.end <= i2.end){
-                    if(swap) r++;
-                    else l++;
-                }
+                assert false;
             }
         }
-
-        //List<Interval> f = new ArrayList<>();
+        ret.add(prev);
         return ret;
     }
 
@@ -83,12 +68,49 @@ public class EmployeeFreeTime_759 {
     @org.junit.Test
     public void test(){
 
-        assertEquals("[[1,3], [5,6]]", mergeInterval(com.sun.tools.javac.util.List.of(new Interval(1,2), new Interval(5,6)),
-                com.sun.tools.javac.util.List.of(new Interval(1,3))).toString());
-        assertEquals("[[1,3], [5,6]]", mergeInterval(com.sun.tools.javac.util.List.of(new Interval(1,2), new Interval(5,6)),
-                com.sun.tools.javac.util.List.of(new Interval(1,5))).toString());
+        ArrayList<Interval> a = new ArrayList<>();
+        a.add(new Interval(1,2));
+        a.add(new Interval(5,6));
+        a.add(new Interval(1,3));
+
+        assertEquals("[[1,3], [5,6]]", mergeInterval(a).toString());
+
+        ArrayList<Interval> b = new ArrayList<>();
+        b.add(new Interval(1,2));
+        b.add(new Interval(5,6));
+        b.add(new Interval(1,5));
+        assertEquals("[[1,6]]", mergeInterval(b).toString());
 
 //        assertEquals("steps", shortestCompletingWord("1s3 PSt",new String[]{"step", "steps", "stripe", "stepple"} ));
 //        assertEquals(false, isOneBitCharacter(new int[]{1,1,1,0}));
+    }
+
+    @Test
+    public void example1(){
+        ArrayList<Interval> a = new ArrayList<>();
+        a.add(new Interval(1,2));
+        a.add(new Interval(5,6));
+        a.add(new Interval(1,3));
+        a.add(new Interval(4,10));
+
+        assertEquals("[[1,3], [4,10]]", mergeInterval(a).toString());
+        List<List<Interval>> t = new ArrayList<>();
+        t.add(a);
+        assertEquals("[[3,4]]", employeeFreeTime(t).toString());
+    }
+
+    @Test
+    public void example2(){
+        ArrayList<Interval> a = new ArrayList<>();
+        a.add(new Interval(1,3));
+        a.add(new Interval(6,7));
+        a.add(new Interval(2,4));
+        a.add(new Interval(2,5));
+        a.add(new Interval(9,12));
+
+        assertEquals("[[1,5], [6,7], [9,12]]", mergeInterval(a).toString());
+        List<List<Interval>> t = new ArrayList<>();
+        t.add(a);
+        assertEquals("[[5,6], [7,9]]", employeeFreeTime(t).toString());
     }
 }
