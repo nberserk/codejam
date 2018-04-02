183c6219f48f0e22f755ec97fde8ac7a3e7dfc84
nberserk <nberserk@gmail.com>
2018-04-03 08:53:04 +0900

    2nd version

--- cpp/codejam/seen/180317TrashBin.cpp
+++ cpp/codejam/seen/180317TrashBin.cpp
@@ -30,6 +30,7 @@ move_trash(y,x,dir)
  my result: 4960 0000
 
  initial try on note9 : 3851
+  2nd version : 5303
 */
 
 //using namespace std;
@@ -123,10 +124,8 @@ void move_trash(int y,int x, int d){
     }else{
         int i=org_trash_map[y][x]-1;
 		hassert(i < 3);
-        if (trash_bin[i]>=3500) {
-            hassert(0);
-            return;
-        }
+		hassert(trash_bin[i] < 3500);
+        
         trash_bin[i]++;
         org_trash_map[oy][ox]=0;
     }
@@ -135,25 +134,29 @@ void move_trash(int y,int x, int d){
 // start of user code
 struct Point{
     int x,y;
+	int bin;
 };
 
 struct Cell{
     int x,y;
     int depth;
     int dir;
+    int binIdx;
     Cell* parent;
     bool visited;
 };
 
-Cell cell[SIZE][SIZE];
-Point queue[2000000];
+Cell cell[SIZE][SIZE][3];
+Point queue[6000000];
 int qs,qe;
 int totalConsumed;
+Point bin[3];
+int consumed[3];
 
 void addQueue(Point& pt){
     queue[qe] = pt;
     qe++;
-    hassert(qe<2000000);
+    hassert(qe<6000000);
 }
 
 Point popQueue(){
@@ -162,32 +165,37 @@ Point popQueue(){
 }
 
 
-void bfs(int m[SIZE][SIZE], Point& ptBin){
+void bfs(int m[SIZE][SIZE]){
     
     for (int y=0; y<SIZE; y++) {
         for (int x=0; x<SIZE; x++) {
-            cell[y][x].visited=false;
+            cell[y][x][0].visited=false;
+			cell[y][x][1].visited = false;
+			cell[y][x][2].visited = false;
         }
     }
 
     qs = qe=0;
-    addQueue(ptBin);
-    Cell* pCell = &cell[ptBin.y][ptBin.x];
-    pCell->parent=0;
-    pCell->x = ptBin.x;
-    pCell->y = ptBin.y;
-    //pCell->visited=true;
+    for(int i=0;i<3;i++){
+        addQueue(bin[i]);
+        Cell* pCell = &cell[bin[i].y][bin[i].x][i];
+        pCell->parent=0;
+        pCell->x = bin[i].x;
+        pCell->y = bin[i].y;
+        pCell->binIdx=i;
+    }    
 
-    int consumed=0;
     int dir[4][2] = {{0,1}, {0,-1},{1,0},{-1,0} };
     while(qe-qs>0){
         Point pt = popQueue();
 
-        Cell& c = cell[pt.y][pt.x];
+        Cell& c = cell[pt.y][pt.x][pt.bin];
         if(c.visited)continue;
         c.visited=true;
-
+		if (consumed[pt.bin] >= 3500)
+			continue;
         if (m[pt.y][pt.x]==-1) {
+			
             // move to bin
             Cell* t = &c;
             while (t->parent!=0) {
@@ -196,10 +204,11 @@ void bfs(int m[SIZE][SIZE], Point& ptBin){
             }
 
 			m[pt.y][pt.x] = 0;
-            consumed++;
+			consumed[pt.bin]++;
+			hassert(consumed[pt.bin] <= 3500);
             totalConsumed++;
             //hprint("consumed(%d,%d), %d,%d \n", pt.x, pt.y, consumed, totalConsumed);
-            if(consumed==3500 || totalConsumed==10000) return;
+            if(totalConsumed==10000) return;
         }
 
         Point np;
@@ -207,25 +216,32 @@ void bfs(int m[SIZE][SIZE], Point& ptBin){
             int nx = pt.x + dir[j][0];
             int ny = pt.y + dir[j][1];
             if(nx<0||ny<0||nx>=SIZE||ny>=SIZE) continue;
-            Cell* nc = &cell[ny][nx];
+            Cell* nc = &cell[ny][nx][pt.bin];
             if(nc->visited || m[ny][nx]>0) continue;
+			
             nc->parent = &c;
             nc->depth = c.depth+1;
             nc->x = nx;
             nc->y = ny;
             nc->dir = j;
+			nc->binIdx = pt.bin;
 
             np.x = nx;
             np.y = ny;
+			np.bin = pt.bin;
             addQueue(np);
         }
     }
 }
 
 void test(int m[SIZE][SIZE]){
-    Point bin[3];
+
     int bi=0;
     totalConsumed=0;
+
+	for (size_t i = 0; i < 3; i++){
+		consumed[i] = 0;
+	}
 
     // find bin
     for (int y=0; y<SIZE; y++) {
@@ -233,17 +249,16 @@ void test(int m[SIZE][SIZE]){
             if (m[y][x]>0) {
                 bin[bi].x = x;
                 bin[bi].y = y;
+				bin[bi].bin = bi;
                 bi++;
                 if(bi==3)break;
             }
         }
         if(bi==3)break;
-    }
+    }			
 
     // bfs
-    for (int i=0; i<bi; i++) {
-        bfs(m, bin[i]);
-    }
+    bfs(m);    
 }
 
 // end of user code
