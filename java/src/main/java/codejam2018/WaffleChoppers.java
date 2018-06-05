87af370f45775e17aed6bf974f7a831262ee5f38
Darren Ha <nberserk@gmail.com>
2018-06-05 11:10:38 +0900

    WC ing --;

--- java/src/main/java/codejam2018/WaffleChoppers.java
+++ java/src/main/java/codejam2018/WaffleChoppers.java
@@ -1,9 +1,13 @@
 package codejam2018;//package codejam2018;
 
+import org.junit.Test;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class WaffleChoppers {
@@ -17,11 +21,10 @@ public class WaffleChoppers {
             int C = in.nextInt();
             int H = in.nextInt();
             int V = in.nextInt();
+
             int[][] m = new int[R][C];
             for (int j = 0; j < R; j++) {
                 String temp = in.next();
-//                System.out.println(temp);
-//                System.out.flush();
                 for (int k = 0; k < C; k++) {
                     m[j][k] = temp.charAt(k)=='.' ? 0:1;
                 }
@@ -29,30 +32,66 @@ public class WaffleChoppers {
 
             int r = solve(R,C,H,V,m);
 
-            if(r==-1)
-                System.out.println("Case #" + i + ": IMPOSSIBLE" );
-            else
-                System.out.println("Case #" + i + ": POSSIBLE" );
+            System.out.println("Case #" + i + (r==-1?": IMPOSSIBLE":": POSSIBLE") );
         }
     }
 
-    static int getSum(int[] s, int start, int end){
-        int ret = s[end];
-        if(start>0){
-            ret-=s[start-1];
+    private static int solve(int R, int C, int H, int V, int[][] m){
+        int chocolate=0;
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                chocolate+=m[r][c];
+            }
         }
-        return ret;
-    }
+        if(chocolate==0) return 0;
+        if(chocolate%( (H+1)*(V+1)) !=0) return -1;
 
-    private static int solve(int R, int C, int H, int V, int[][] m) {
-        int chocolate =0;
-
-        int[][] sum = new int[R][C];
+        int count=0;
+        int k=0;
+        int[] hi= new int[R];
+        int target = chocolate/(H+1);
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
-                if(m[i][j]==1) chocolate++;
-                if(j>0)
-                    sum[i][j]=sum[i][j-1] + m[i][j];
+                count+=m[i][j];
+            }
+            hi[i]=k;
+            if(count>(k+1)*target) return -1;
+            if(count==(k+1)*target) k++;
+        }
+
+        // why not check k
+        if(k!=H+1) return -1;
+
+
+        target = chocolate/((H+1)*(V+1));
+        int[] countH = new int[H+1];
+
+        k=0;
+        for (int i = 0; i < C; i++) {
+            for (int j = 0; j < R; j++) {
+                countH[hi[j]] +=m[j][i];
+            }
+
+            int hitCount=0;
+            for (int j = 0; j < H + 1; j++) {
+                if (countH[j]>(k+1)*target) return -1;
+                if (countH[j]==(k+1)*target) hitCount++;
+            }
+            if(hitCount==H+1) k++;
+        }
+
+        if(k!=V+1) return -1;
+
+
+        return 0;
+    }
+
+
+    private static int solve1(int R, int C, int H, int V, int[][] m) {
+        int chocolate=0;
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                chocolate+=m[r][c];
             }
         }
         if(chocolate==0) return 0;
@@ -61,9 +100,13 @@ public class WaffleChoppers {
         int target = chocolate/(H+1);
 
         ArrayList<Integer> hsplit = new ArrayList<>();
+
         int count=0;
         for (int i = 0; i < R; i++) {
-            count += getSum(sum[i],0, C-1);
+            for (int j = 0; j < C; j++) {
+                count+=m[i][j];
+            }
+
             if(count>target) return -1;
             if(count ==target) {
                 hsplit.add(i);
@@ -71,73 +114,103 @@ public class WaffleChoppers {
             }
         }
 
-        if(hsplit.size()!=H) return -1;
+        if(hsplit.size()!=H+1) return -1;
 
         // first row
-        target/=(V+1);
-        count=0;
+        target= chocolate/((H+1)*(V+1));
+
         ArrayList<Integer> vsplit = new ArrayList<>();
-        for (int i = 0; i < C; i++) {
-            for (int j = 0; j < hsplit.get(0); j++) {
-                count+=getSum(sum[j],j-1,j);
+        count = 0;
+        for (int c = 0; c < C; c++) {
+            for (int i = 0; i <= hsplit.get(0); i++) {
+                count +=m[i][c];
             }
             if(count > target) return -1;
             if(count==target){
-                count==0;
-                vsplit.add(i);
+                vsplit.add(c);
+                count=0;
             }
         }
 
-        if(vsplit.size()!=V) return -1;
+        if(vsplit.size()!=V+1) return -1;
 
-        for (int i = 1; i < hsplit.size(); i++) {
-            int sr = hsplit.get(i-1);
-            int er = hsplit.get(i);
+        for (int hi = 1; hi < H+1; hi++) {
+            int rs=hsplit.get(hi-1)+1;
+            int re=hsplit.get(hi);
 
+            for (int vi = 0; vi < V+1; vi++) {
+                int cs = vi==0? 0 : vsplit.get(vi-1)+1;
+                int ce = vsplit.get(vi);
+
+                count=0;
+                for (int r = rs; r <= re; r++) {
+                    for (int c = cs; c <= ce; c++) {
+                        count+=m[r][c];
+                    }
+                }
+
+                if(count!=target)return -1;
+            }
         }
-
 
 
         return 0;
     }
 
 
-//    @Test
-//    public void test(){
-//        int[][] m3 = new int[][]{
-//                {0,0,1,1},
-//                {0,0,1,1},
-//                {1,1,0,0},
-//                {1,1,0,0}
-//
-//        };
-//        assertEquals(-1, solve(4,4,1,1, m3));
-//
-//        int[][] m = new int[][]{
-//            {0,1,1,0,0,1},
-//            {0,0,0,0,0,1},
-//            {1,0,1,0,1,1}
-//        };
-//        assertEquals(0, solve(3,6,1,1, m));
-//
-//        int[][] m2 = new int[][]{
-//                {0,0,0,0,0},
-//                {0,0,0,0,0},
-//                {0,0,0,0,0},
-//                {0,0,0,0,0},
-//                {0,0,0,0,0}
-//        };
-//        assertEquals(0, solve(4,5,1,1, m2));
-//
-//
-//        int[][] m4 = new int[][]{
-//                {0,1,0,1},
-//                {1,0,1,0},
-//                {0,1,0,1},
-//        };
-//        assertEquals(-1, solve(3,4,1,2, m4));
-//
-//    }
+    @Test
+    public void test(){
+        int[][] m3 = new int[][]{
+                {0,0,1,1},
+                {0,0,1,1},
+                {1,1,0,0},
+                {1,1,0,0}
+
+        };
+        assertEquals(-1, solve(4,4,1,1, m3));
+
+        int[][] m = new int[][]{
+            {0,1,1,0,0,1},
+            {0,0,0,0,0,1},
+            {1,0,1,0,1,1}
+        };
+        assertEquals(0, solve(3,6,1,1, m));
+
+        int[][] m2 = new int[][]{
+                {0,0,0,0,0},
+                {0,0,0,0,0},
+                {0,0,0,0,0},
+                {0,0,0,0,0},
+                {0,0,0,0,0}
+        };
+        assertEquals(0, solve(4,5,1,1, m2));
+
+
+        int[][] m4 = new int[][]{
+                {0,1,0,1},
+                {1,0,1,0},
+                {0,1,0,1},
+        };
+        assertEquals(-1, solve(3,4,1,2, m4));
+
+        int[][] m5 = new int[][]{
+            {1,0,1,1},
+            {1,1,0,1},
+            {1,0,1,1},
+        };
+        assertEquals(0, solve(3,4,2,2, m5));
+
+
+
+        int[][] m6 = new int[][]{
+            {1,1,1},
+            {1,0,1},
+            {1,0,1},
+            {1,1,1}
+        };
+        assertEquals(-1, solve(4,3,1,1, m6));
+
+    }
 
 
 }
