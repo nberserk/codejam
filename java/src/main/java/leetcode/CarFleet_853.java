9a60753f97fc661a4faf6edaa79ce3d52fb1ed92
Darren Ha <nberserk@gmail.com>
2018-06-21 19:44:05 +0900

    853 ing

--- java/src/main/java/leetcode/CarFleet_853.java
+++ java/src/main/java/leetcode/CarFleet_853.java
@@ -1,7 +1,9 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,34 +15,26 @@ public class CarFleet_853 {
     }
 
     public int carFleet(int target, int[] position, int[] speed) {
-
-        ArrayList<Fleet> cars = new ArrayList<>();
         int N = position.length;
+        Integer[] a = new Integer[N];
         for (int i = 0; i < N; i++) {
-            for (int j = i+1; j < N; j++) {
-                if (speed[i]==speed[j]) continue;
-                double x = Math.floor( (position[j]-position[i])/(double)(speed[i]-speed[j] ));
-
-                if(x<=0||x>target) continue;
-
-                Fleet f = new Fleet();
-                f.time= (int) x;
-                if (speed[i]>speed[j]){
-                    f.live=j;
-                    f.dead=i;
-                }else {
-                    f.live=i;
-                    f.dead=j;
-                }
-                cars.add(f);
-            }
+            a[i]=i;
         }
-        Collections.sort(cars, (a,b)->a.time-b.time);
 
+        Arrays.sort(a, (x,b)->position[x]-position[b]);
         boolean[] dead = new boolean[N];
-        for (int i = 0; i < cars.size(); i++) {
-            Fleet f = cars.get(i);
-            dead[f.dead] = true;
+        for (int i = N-2; i >=0; i--) {
+            int ii = a[i];
+            for (int j = i+1; j < N; j++) {
+                int jj = a[j];
+                if (dead[jj] || speed[ii]==speed[jj])continue;
+                double cross = (position[jj]-position[ii])/(double)(speed[ii]-speed[jj]);
+                double y = position
+                if(cross>=0 && cross<=target){
+                    dead[ii]=true;
+                    break;
+                }
+            }
         }
 
         int ret=0;
@@ -54,6 +48,7 @@ public class CarFleet_853 {
     @org.junit.Test
     public void test(){
         assertEquals(3, carFleet(12, new int[]{10, 8, 0,5,3}, new int[]{2,4,1,1,3}));
+        assertEquals(2, carFleet(10, new int[]{6,8}, new int[]{3,2}));
 //        assertEquals(true, isOneBitCharacter(new int[]{1,0,0}));
 //        assertEquals(false, isOneBitCharacter(new int[]{1,1,1,0}));
     }
