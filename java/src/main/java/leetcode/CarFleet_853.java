65a43a961e6f0076dce2581fe11cfe034e842fd4
Darren Ha <nberserk@gmail.com>
2018-06-22 10:12:51 +0900

    CarFleet done

--- java/src/main/java/leetcode/CarFleet_853.java
+++ java/src/main/java/leetcode/CarFleet_853.java
@@ -10,11 +10,42 @@ import static org.junit.Assert.assertEquals;
 public class CarFleet_853 {
 
     static class Fleet{
-        int time;
-        int live, dead;
+        double time;
+        int p,s;
     }
 
+    // O(NlogN)
     public int carFleet(int target, int[] position, int[] speed) {
+        int N = position.length;
+        if(N==0) return 0;
+        if(N==1) return 1;
+
+        Fleet[] fleets = new Fleet[N];
+        for (int i = 0; i < N; i++) {
+            Fleet f = new Fleet();
+            f.p = position[i];
+            f.s = speed[i];
+            f.time = (target-position[i])/(double)f.s;
+            fleets[i]=f;
+        }
+        Arrays.sort(fleets, (a,b)->a.p-b.p);
+        int ret=0;
+
+        int i=N-2;
+        int iFront=N-1;
+        while(i>=0){
+            if (fleets[i].time>fleets[iFront].time){//i and iFront can't meet. becuase i is much slower
+                ret++;
+                iFront=i;
+            }
+            i--;
+        }
+
+        return ret + 1;
+    }
+
+    // O(N^2) bad
+    public int carFleet_try1(int target, int[] position, int[] speed) {
         int N = position.length;
         Integer[] a = new Integer[N];
         for (int i = 0; i < N; i++) {
@@ -27,11 +58,15 @@ public class CarFleet_853 {
             int ii = a[i];
             for (int j = i+1; j < N; j++) {
                 int jj = a[j];
-                if (dead[jj] || speed[ii]==speed[jj])continue;
+                if (dead[jj] || speed[ii]<=speed[jj])continue;
                 double cross = (position[jj]-position[ii])/(double)(speed[ii]-speed[jj]);
-                double y = position
-                if(cross>=0 && cross<=target){
-                    dead[ii]=true;
+                if(cross<=0) continue;
+                double y = position[jj]+cross*speed[jj];
+                if(y>=0 && y<=target){
+                    if (speed[ii]<speed[jj])
+                        dead[jj]=true;
+                    else
+                        dead[ii]=true;
                     break;
                 }
             }
@@ -49,6 +84,7 @@ public class CarFleet_853 {
     public void test(){
         assertEquals(3, carFleet(12, new int[]{10, 8, 0,5,3}, new int[]{2,4,1,1,3}));
         assertEquals(2, carFleet(10, new int[]{6,8}, new int[]{3,2}));
+        assertEquals(1, carFleet(10, new int[]{0,4,2}, new int[]{2,1,3}));
 //        assertEquals(true, isOneBitCharacter(new int[]{1,0,0}));
 //        assertEquals(false, isOneBitCharacter(new int[]{1,1,1,0}));
     }
