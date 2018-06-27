c8fb1f7c8fe875a4d36bbab666d2fd346aae4049
Darren Ha <nberserk@gmail.com>
2018-06-27 10:32:18 +0900

    refine 97

--- java/src/main/java/leetcode/InterleavingString_97.java
+++ java/src/main/java/leetcode/InterleavingString_97.java
@@ -2,8 +2,6 @@ package leetcode;
 
 import org.junit.Assert;
 
-import java.util.Arrays;
-
 
 public class InterleavingString_97 {
     public boolean isInterleave(String s1, String s2, String s3) {
@@ -14,34 +12,25 @@ public class InterleavingString_97 {
 
         int MN = Math.max(N,M)+1;
 
-        int[][] cache = new int[MN][MN];//1:not possible, 2:possible
-        Arrays.fill(cache[N], 1);
-        for (int i = 0; i < N; i++) {
-            cache[i][M]=1;
-        }
-        cache[N][M]=2;
+        boolean[][] cache = new boolean[N+1][M+1];//1:not possible, 2:possible
 
-        int T=N+M;
-        for (int l = T-1; l >=0; l--) {
-            for (int n = 0; n <= l; n++) {
-                int m = l-n;
-                if(n>N || m>M) {
-                    //cache[n][m]=1;
-                    continue;
+        for (int n = 0; n <=N ; n++) {
+            for (int m = 0; m <= M; m++) {
+                if (n==0 && m==0)
+                    cache[n][m]=true;
+                else{
+                    boolean v = false;
+                    if (n>0 && s1.charAt(n-1)==s3.charAt(n+m-1)){
+                        v=cache[n-1][m];
+                    }
+                    if(v==false && m>0 && s2.charAt(m-1)==s3.charAt(n+m-1)){
+                        v=cache[n][m-1];
+                    }
+                    cache[n][m]=v;
                 }
-                int v = 1;
-                if(n<N&& s1.charAt(n)==s3.charAt(l)){
-                    v = cache[n+1][m];
-                }
-
-                if(v==1 && m<M && s2.charAt(m)==s3.charAt(l)){
-                    v = cache[n][m+1];
-                }
-                cache[n][m] = v;
             }
         }
-
-        return cache[0][0]==2;
+        return cache[N][M];
     }
 
     @org.junit.Test
@@ -50,6 +39,5 @@ public class InterleavingString_97 {
         Assert.assertEquals(true, isInterleave("ab", "cd", "acbd"));
         Assert.assertEquals(true, isInterleave("bcc", "dbbca", "dbbcbcac"));
         Assert.assertEquals(false, isInterleave("aabcc", "dbbca", "aadbbbaccc"));
-
     }
 }
