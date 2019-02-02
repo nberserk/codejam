5e2b7ea0f797aa0f50fe6998a7c2ef20c585a7f8
Darren ha <nberserk@gmail.com>
2019-02-02 12:29:31 +0900

    588 done

--- java/src/main/java/leetcode/LC588.java
+++ java/src/main/java/leetcode/LC588.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -13,60 +14,73 @@ import java.util.List;
 public class LC588 {
 
     static class Node{
-        HashMap<String, Node> child = new HashMap<>();
-        String content="";
-        boolean isDir;
+        String name;
+        public HashMap<String, Node> child = new HashMap<>();
+        String content;
+        public Node(String n){
+            this.name = n;
+        }
     }
+
 
     Node root;
     public LC588() {
-        root = new Node();
-        root.isDir=true;
+        root = new Node("");
     }
 
     public List<String> ls(String path) {
         List<String> ret = new ArrayList<>();
-        String[] dir = path.substring(1).split("/");
+        String[] dir = path.split("/");
 
         Node cur = root;
-        for (String d: dir){
-            Node c = cur.child.get(d);
-            if(c==null) break;
-            cur = c;
+        for (int i = 1; i < dir.length; i++) {
+            String n = dir[i];
+            Node next = cur.child.get(n);
+            if(next==null) break;
+            cur = next;
         }
-        for (String k : cur.child.keySet()){
-            ret.add(k);
+
+        if (cur.child.size()>0){
+            for ( String key: cur.child.keySet()){
+                ret.add(key);
+            }
+            Collections.sort(ret);
+        }else{
+            if(cur.name.length()>0)
+                ret.add(cur.name);
         }
         return ret;
     }
 
     public void mkdir(String path) {
-        String[] dir = path.substring(1).split("/");
+        String[] dir = path.split("/");
 
         Node cur = root;
-        for(String s: dir){
-            Node c = cur.child.get(s);
-            if(c==null){
-                c = new Node();
-                c.isDir=true;
-                cur.child.put(s, c);
+        for (int i = 1; i < dir.length; i++) {
+            String k = dir[i];
+            Node next = cur.child.get(k);
+            if(next==null) {
+                next = new Node(k);
+                cur.child.put(k, next);
             }
-            cur=c;
+            cur = next;
         }
     }
 
     public void addContentToFile(String filePath, String content) {
-        String[] dir = filePath.substring(1).split("/");
+        String[] dir = filePath.split("/");
 
         Node cur = root;
-        for (int i = 0; i < dir.length ; i++) {
+        for (int i = 1; i < dir.length ; i++) {
             Node c = cur.child.get(dir[i]);
             if(c==null){
-                c = new Node();
+                c = new Node(dir[i]);
                 cur.child.put(dir[i], c);
             }
             cur = c;
         }
+        if(cur.content==null)
+            cur.content="";
         cur.content +=content;
     }
 
@@ -86,17 +100,12 @@ public class LC588 {
 
     @Test
     public void test(){
-
         LC588 l = new LC588();
         List<String> t = l.ls("/") ;
         l.mkdir("/a/b/c"); ;
         l.addContentToFile("/a/b/c/d", "hello");
+        Assert.assertEquals(1, l.ls("/").size());
         String content = l.readContentFromFile("/a/b/c/d");
         Assert.assertEquals("hello", content);
-
-
-
-
-
     }
 }
