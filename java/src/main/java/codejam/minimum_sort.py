961a437272cc967fdff17bc2bc9c2bac10136b05
daesung <nberserk@gmail.com>
2021-05-17 19:59:22 -0700

    miniumum sort done

---
+++ java/src/main/java/codejam/minimum_sort.py
@@ -0,0 +1,28 @@
+t, n  = [int(s) for s in input().split(" ")] # read a list of integers, 2 in this case
+
+def sort2(start, end):
+    for i in range(start, end):
+        Output("M {} {}".format(i, end))
+        minIdx = int(input())
+        if(minIdx!=i):
+            Output("S {} {}".format(i, minIdx))
+            minIndex = int(input())
+
+def Output(line):
+    try:
+        print(line)
+        sys.stdout.flush()
+    except:
+        # If we let stdout be closed by the end of the program, then an unraisable
+        # broken pipe exception will happen, and we won't be able to finish
+        # normally.
+        try:
+            sys.stdout.close()
+        except:
+            pass
+
+for i in range(1, t + 1):
+    sort2(1, n)
+    Output("D")
+    ret= int(input())
+
